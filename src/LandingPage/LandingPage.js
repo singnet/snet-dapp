db3e5725cd6cb75d454d69906686e806dc704188
chetankothari <chetankothari@users.noreply.github.com>
2019-02-27 22:26:06 +0530

    Change CRE default values

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,25 +3,11 @@ import React, { Component } from 'react';
 class LandingPage extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        Welcome to SingularityNET
       </div>
     );
   }
 }
-
-LandingPage.propTypes = {};
 
 export default LandingPage;
