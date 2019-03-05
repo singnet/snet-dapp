06e034af2c8a377196901e1fc6f94e25b860ae6b
chetankothari <chetankothari@users.noreply.github.com>
2019-03-05 19:27:05 +0530

    Add react-testing-library

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,11 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// react-testing-library renders your components to document.body,
+// this will ensure they're removed after each test.
+import 'react-testing-library/cleanup-after-each';
+
+// this adds jest-dom's custom assertions
+import 'jest-dom/extend-expect';
+
 configure({ adapter: new Adapter() });
