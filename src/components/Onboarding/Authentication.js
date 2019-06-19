a68335a246ec8342eed735f05b50e71dee5c50c6
Vivek205 <n.vin95@gmail.com>
2019-06-19 14:39:42 +0530

    seggregated Authentication section

---
+++ src/components/Onboarding/Authentication.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import StyledButton from "../common/StyledButton";
+import { withStyles } from "@material-ui/styles";
+
+const useStyles = theme => ({
+  authenticationContent: {
+    width: "41%",
+    paddingBottom: 40,
+    margin: "40px auto 0",
+    backgroundColor: theme.palette.text.white,
+    boxShadow:
+      "0 1px 1px 0 rgba(0,0,0,0.14), 0 2px 1px -1px rgba(0,0,0,0.14), 0 1px 3px 0 rgba(0,0,0,0.2)",
+    textAlign: "center",
+    "& h3": {
+      padding: "15px 0 15px 25px",
+      borderBottomWidth: 1,
+      borderBottomStyle: "solid",
+      borderBottomColor: theme.palette.text.gray5,
+      margin: 0,
+      color: theme.palette.text.black1,
+      fontSize: 20,
+      textAlign: "left"
+    },
+    "& p": {
+      padding: "30px 50px 20px",
+      margin: 0,
+      color: theme.palette.text.gray10,
+      fontSize: 14,
+      fontFamily: theme.typography.secondary.main,
+      lineHeight: "21px",
+      textAlign: "left",
+      "& span": {
+        fontWeight: theme.typography.fontweight
+      }
+    }
+  },
+  pendingSection: {
+    marginBottom: 20,
+    color: theme.palette.text.orange,
+    fontWeight: theme.typography.fontweight,
+    "& i": {
+      fontSize: 12,
+      paddingRight: 10
+    }
+  },
+  textField: {
+    width: "60%",
+    margin: 0,
+    "& label": {
+      color: "rgba(0,0,0,.87) !important"
+    }
+  },
+  buttonsContainer: {
+    marginTop: 40,
+    "& button": {
+      padding: " 13px 60px 11px"
+    }
+  }
+});
+
+class Authentication extends Component {
+  state = {
+    verificationCode: ""
+  };
+  render() {
+    const { classes } = this.props;
+    const { verificationCode } = this.state;
+    return (
+      <div className={classes.authenticationContent}>
+        <h3>Validate Email</h3>
+        <p>
+          <span>
+            A verification code has been sent to your registered email address.
+          </span>
+          <br /> <br />
+          Please enter the verification code below to confirm your email
+          address. If you are unable to find the email from{" "}
+          <span>‘otp@singularitynet.io’</span> in your inbox, make sure to check
+          the spam folder. The code will be valid for 5 minutes.{" "}
+        </p>
+        <div className={classes.pendingSection}>
+          <i className="far fa-hourglass"></i>
+          <span>Pending</span>
+        </div>
+        <TextField
+          id="outlined-verification-code"
+          label="Verification Code"
+          className={classes.textField}
+          type="text"
+          name="verificationCode"
+          margin="normal"
+          variant="outlined"
+          value={verificationCode}
+          onChange={this.handleUsername}
+        />
+        <span></span>
+        <div className={classes.buttonsContainer}>
+          <StyledButton
+            btnText="resend code"
+            type="transparent"
+            disabled={false}
+          />
+          <StyledButton btnText="continue" disabled />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(useStyles)(Authentication);
