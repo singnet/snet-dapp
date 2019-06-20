a8e6e90f71ae3c84ef958d8a5a6529b09e5dd4fc
Vivek205 <n.vin95@gmail.com>
2019-06-20 15:10:12 +0530

    converted AIMarketplace into class components

--- src/components/AiMarketplace/index.js
+++ src/components/AiMarketplace/index.js
@@ -7,10 +7,10 @@ import StyledButton from "../common/StyledButton/index.js";
 import MainSection from "./MainSection/index.js";
 
 // Material UI imports
-import { makeStyles } from "@material-ui/styles";
 import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = theme => ({
   mainWrapper: {
     width: "92%",
     margin: "0 auto"
@@ -33,44 +33,46 @@ const useStyles = makeStyles(theme => ({
     fontSize: 24,
     lineHeight: "29px"
   }
-}));
+});
 
-function AiMarketplace() {
-  const classes = useStyles();
-  return (
-    <div>
-      <Header />
-      <div className={classes.mainWrapper}>
-        <Grid container spacing={24}>
-          <Grid
-            item
-            xs={12}
-            sm={3}
-            md={3}
-            lg={3}
-            className={classes.titleContainer}
-          >
-            <h2 className={classes.title}>AI Marketplace</h2>
+class AiMarketplace extends Component {
+  render() {
+    const classes = useStyles();
+    return (
+      <div>
+        <Header />
+        <div className={classes.mainWrapper}>
+          <Grid container spacing={24}>
+            <Grid
+              item
+              xs={12}
+              sm={3}
+              md={3}
+              lg={3}
+              className={classes.titleContainer}
+            >
+              <h2 className={classes.title}>AI Marketplace</h2>
+            </Grid>
+            <Grid item xs={12} sm={9} md={9} lg={9}>
+              <p className={classes.description}>
+                Want to find the right AI service for your project? You’ve come
+                to the right place. <br />
+                We’ve got a growing marketplace with hundreds of AI services for
+                you to utilize. <br />
+                They’re powered by a community of amazing developers from all
+                over the globe.
+              </p>
+              <StyledButton type="blue" btnText="Sign up for free credits" />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={9} md={9} lg={9}>
-            <p className={classes.description}>
-              Want to find the right AI service for your project? You’ve come to
-              the right place. <br />
-              We’ve got a growing marketplace with hundreds of AI services for
-              you to utilize. <br />
-              They’re powered by a community of amazing developers from all over
-              the globe.
-            </p>
-            <StyledButton type="blue" btnText="Sign up for free credits" />
-          </Grid>
-        </Grid>
-        <div>
-          <MainSection />
+          <div>
+            <MainSection />
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
 
-export default AiMarketplace;
+export default withStyles(useStyles)(AiMarketplace);
