54f22a223110b190377882f94f5f12d2521ac2a6
dasari-ananya <ananya.dasari@nirman.io>
2019-06-25 13:39:02 +0530

    separated expansion panel component inside filter component

--- src/components/AiMarketplace/MainSection/Filter/index.js
+++ src/components/AiMarketplace/MainSection/Filter/index.js
@@ -1,11 +1,6 @@
 import React from "react";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+
+import StyledExpansionPanel from "./StyledExpansionPanel.js";
 
 import { useStyles } from "./styles";
 
@@ -19,87 +14,7 @@ const Filter = () => {
                     Reset
                 </button>
             </div>
-            <div>
-                <ExpansionPanel className={classes.filterExpansionPanel}>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography className={classes.filtersHeadingTitle}>Categories</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails className={classes.filterDetails}>
-                        <div className={classes.formCntrlGrup}>
-                            <FormControlLabel
-                                control={<Checkbox checked={true} value="textAnalysis" />}
-                                label="Text Analysis"
-                                classes={{
-                                    label: classes.checkboxLabel,
-                                }}
-                            />
-                            <span className={classes.count}>23</span>
-                        </div>
-                        <div className={classes.formCntrlGrup}>
-                            <FormControlLabel
-                                control={<Checkbox checked={false} value="computerVision" />}
-                                label="Computer Vision"
-                                classes={{
-                                    label: classes.checkboxLabel,
-                                }}
-                            />
-                            <span className={classes.count}>45</span>
-                        </div>
-                        <div className={classes.formCntrlGrup}>
-                            <FormControlLabel
-                                control={<Checkbox checked={false} value="deepLearning" />}
-                                label="Deep Learning"
-                                classes={{
-                                    label: classes.checkboxLabel,
-                                }}
-                            />
-                            <span className={classes.count}>14</span>
-                        </div>
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-
-                <ExpansionPanel className={classes.filterExpansionPanel}>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography className={classes.filtersHeadingTitle}>Price</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails className={classes.filterDetails}>
-                        <FormControlLabel
-                            control={<Checkbox checked={false} value="textAnalysis" />}
-                            label="Text Analysis"
-                            classes={{
-                                label: classes.checkboxLabel,
-                            }}
-                        />
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-
-                <ExpansionPanel className={classes.filterExpansionPanel}>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography className={classes.filtersHeadingTitle}>Vendors</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails className={classes.filterDetails}>
-                        <FormControlLabel
-                            control={<Checkbox checked={false} value="textAnalysis" />}
-                            label="Text Analysis"
-                            classes={{
-                                label: classes.checkboxLabel,
-                            }}
-                        />
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-            </div>
+            <StyledExpansionPanel />
         </div>
     );
 };
