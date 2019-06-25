1abe3c10d587efac808a67047c026fecf3786a3e
Vivek205 <n.vin95@gmail.com>
2019-06-25 15:33:16 +0530

    Code splitting using React.lazy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import Amplify from "aws-amplify";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
@@ -7,14 +7,13 @@ import Routes from "./utility/stringConstants/Routes";
 import ForgotPasswordSubmit from "./components/Login/ForgotPasswordSubmit";
 import { aws_config } from "./aws_config";
 import theme from "./assets/Theme";
-import asyncComponent from "./components/common/AsyncComponent";
 
-const ForgotPassword = asyncComponent(() => import("./components/Login/ForgotPassword"));
-const Onboarding = asyncComponent(() => import("./components/Onboarding"));
-const PageNotFound = asyncComponent(() => import("./components/PageNotFound"));
-const AiMarketplace = asyncComponent(() => import("./components/AiMarketplace"));
-const SignUp = asyncComponent(() => import("./components/Login/Signup"));
-const Login = asyncComponent(() => import("./components/Login"));
+const ForgotPassword = lazy(() => import("./components/Login/ForgotPassword"));
+const Onboarding = lazy(() => import("./components/Onboarding"));
+const PageNotFound = lazy(() => import("./components/PageNotFound"));
+const AiMarketplace = lazy(() => import("./components/AiMarketplace"));
+const SignUp = lazy(() => import("./components/Login/Signup"));
+const Login = lazy(() => import("./components/Login"));
 
 Amplify.configure(aws_config);
 
@@ -23,16 +22,18 @@ class App extends Component {
         return (
             <ThemeProvider theme={theme}>
                 <Router>
-                    <Switch>
-                        <Route path={`/${Routes.SIGNUP}`} component={SignUp} />
-                        <Route path={`/${Routes.LOGIN}`} component={Login} />
-                        <Route path={`/${Routes.FORGOT_PASSWORD}`} component={ForgotPassword} />
-                        <Route path={`/${Routes.FORGOT_PASSWORD_SUBMIT}`} component={ForgotPasswordSubmit} />
-                        <Route path={`/${Routes.ONBOARDING}`} component={Onboarding} />
-                        <Route path={`/${Routes.AI_MARKETPLACE}`} component={AiMarketplace} />
-                        <Route path="/" exact component={AiMarketplace} />
-                        <Route component={PageNotFound} />
-                    </Switch>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route path={`/${Routes.SIGNUP}`} component={SignUp} />
+                            <Route path={`/${Routes.LOGIN}`} component={Login} />
+                            <Route path={`/${Routes.FORGOT_PASSWORD}`} component={ForgotPassword} />
+                            <Route path={`/${Routes.FORGOT_PASSWORD_SUBMIT}`} component={ForgotPasswordSubmit} />
+                            <Route path={`/${Routes.ONBOARDING}`} component={Onboarding} />
+                            <Route path={`/${Routes.AI_MARKETPLACE}`} component={AiMarketplace} />
+                            <Route path="/" exact component={AiMarketplace} />
+                            <Route component={PageNotFound} />
+                        </Switch>
+                    </Suspense>
                 </Router>
             </ThemeProvider>
         );
