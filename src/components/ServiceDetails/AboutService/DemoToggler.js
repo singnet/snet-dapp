14f0faee4c090b237522e3c97a4b857d0ae0cce1
Vivek205 <n.vin95@gmail.com>
2019-07-03 14:49:04 +0530

    Added Links to the DemoToggle, converted AboutService to functional component, Demo will be toggled based on the isLOggedIn props

--- src/components/ServiceDetails/AboutService/DemoToggler.js
+++ src/components/ServiceDetails/AboutService/DemoToggler.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import StyledButton from "../../common/StyledButton";
 import DemoExample from "./DemoExample";
+import Routes from "../../../utility/constants/Routes";
 
-const DemoToggler = ({ showDemo }) => {
+const DemoToggler = ({ showDemo, onClick }) => {
   if (showDemo) {
     return <DemoExample />;
   }
   return (
     <div>
-      <StyledButton btnText="login" type="transparent" onClick={this.handleSubmit} />
-      <StyledButton btnText="signup and run for free" />
+      <Link to={Routes.LOGIN}>
+        <StyledButton btnText="login" type="transparent" onClick={onClick} />
+      </Link>
+      <Link to={Routes.SIGNUP}>
+        <StyledButton btnText="signup and run for free" />
+      </Link>
     </div>
   );
 };
