b17eab8234647c45bbabbc3347a5f45406424943
dasari-ananya <ananya.dasari@nirman.io>
2019-07-04 14:15:46 +0530

    removed unwanted imports

---
+++ src/components/UserProfile/UserMenu/UserMenuItem/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Icon from "@material-ui/core/Icon";
+import clsx from "clsx";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/styles";
+
+import { useStyles } from "./styles";
+
+const UserMenuItem = ({ classes, icon, title, linkTo }) => {
+  return (
+    <li>
+      <Icon className={clsx(classes.icon, icon)} />
+      <Link>{title}</Link>
+    </li>
+  );
+};
+
+export default withStyles(useStyles)(UserMenuItem);
