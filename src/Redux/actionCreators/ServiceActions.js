14588c3b468899b03e018a03befecdc2c5e0f5ae
Vivek205 <n.vin95@gmail.com>
2019-07-05 14:56:40 +0530

    moved Async calls to redux

--- src/Redux/actionCreators/ServiceActions.js
+++ src/Redux/actionCreators/ServiceActions.js
@@ -1,7 +1,12 @@
+import { Root } from "protobufjs";
+
 import { APIEndpoints } from "../../utility/constants/APIEndpoints";
+import GRPCProtoV3Spec from "../../assets/models/GRPCProtoV3Spec";
 
 export const UPDATE_SERVICE_LIST = "SET_SERVICE_LIST";
 export const UPDATE_PAGINATION_DETAILS = "SET_PAGINATION_DETAILS";
+export const UPDATE_SERVICE_EXECUTION_RESPONSE = "UPDATE_SERVICE_EXECUTION_RESPONSE";
+export const UPDATE_SPEC_DETAILS = "UPDATE_SPEC_DETAILS";
 
 export const fetchService = pagination => async dispatch => {
   let url = new URL(`${APIEndpoints.GET_SERVICES_LIST.endpoint}/service`);
@@ -17,6 +22,30 @@ export const fetchService = pagination => async dispatch => {
       });
       dispatch({ type: UPDATE_SERVICE_LIST, payload: res.data.result });
     });
+};
+
+export const executeService = (url, data) => dispatch => {
+  return fetch(url, {
+    method: "POST",
+    mode: "CORS",
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(data => dispatch({ type: UPDATE_SERVICE_EXECUTION_RESPONSE, payload: data }));
+};
+
+export const fetchSpecDetails = servicebufURL => dispatch => {
+  return fetch(encodeURI(servicebufURL))
+    .then(serviceSpecResponse => serviceSpecResponse.json())
+    .then(
+      serviceSpec =>
+        new Promise(resolve => {
+          const serviceSpecJSON = Root.fromJSON(serviceSpec[0]);
+          const protoSpec = new GRPCProtoV3Spec(serviceSpecJSON);
+          this.setState({ serviceSpecJSON, protoSpec });
+          resolve({ serviceSpecJSON, protoSpec });
+        })
+    );
 };
 
 export const updatePagination = pagination => dispatch => {
