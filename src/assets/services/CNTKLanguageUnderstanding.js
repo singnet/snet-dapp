fb0b50cdceea5f2b8e0791ee187f61924288b184
Vivek205 <n.vin95@gmail.com>
2019-07-05 12:33:38 +0530

    fixed the imports

--- src/assets/services/CNTKLanguageUnderstanding.js
+++ src/assets/services/CNTKLanguageUnderstanding.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { hasOwnDefinedProperty } from "../../util";
+import { hasOwnDefinedProperty } from "../../utility/JSHelper";
 import Button from "@material-ui/core/Button";
 
 export default class CNTKLanguageUnderstanding extends React.Component {
