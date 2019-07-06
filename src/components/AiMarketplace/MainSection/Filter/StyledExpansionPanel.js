544132b18669beb1fa39a3a3b2e8e1f15b0e793e
Vivek205 <n.vin95@gmail.com>
2019-07-06 17:30:57 +0530

    connverted styledExpansionPanel to statelessComponent

--- src/components/AiMarketplace/MainSection/Filter/StyledExpansionPanel.js
+++ src/components/AiMarketplace/MainSection/Filter/StyledExpansionPanel.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
@@ -10,89 +10,32 @@ import { withStyles } from "@material-ui/styles";
 
 import { useStyles } from "./styles";
 
-class StyledExpansionPanel extends Component {
-  state = {
-    expansionData: [
-      {
-        title: "",
-        items: [
-          {
-            title: "",
-            count: "",
-          },
-          {
-            title: "",
-            count: "",
-          },
-          {
-            title: "",
-            count: "",
-          },
-        ],
-      },
-      {
-        title: "",
-        items: [
-          {
-            title: "",
-            count: "",
-          },
-          {
-            title: "",
-            count: "",
-          },
-          {
-            title: "",
-            count: "",
-          },
-        ],
-      },
-      {
-        title: "",
-        items: [
-          {
-            title: "",
-            count: "",
-          },
-          {
-            title: "",
-            count: "",
-          },
-        ],
-      },
-    ],
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { expansionData } = this.state;
-
-    return (
-      <Fragment>
-        {expansionData.map(val => (
-          <ExpansionPanel className={classes.filterExpansionPanel} key={val.title}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
-              <Typography className={classes.filtersHeadingTitle}>{val.title}</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails className={classes.filterDetails}>
-              {val.items.map(item => (
-                <div className={classes.formCntrlGrup} key={item.title}>
-                  <FormControlLabel
-                    control={<Checkbox checked={true} value="textAnalysis" />}
-                    label={item.title}
-                    classes={{
-                      label: classes.checkboxLabel,
-                    }}
-                  />
-                  <span className={classes.count}>{item.count}</span>
-                </div>
-              ))}
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        ))}
-      </Fragment>
-    );
-  }
-}
+const StyledExpansionPanel = ({ classes, expansionData }) => {
+  return (
+    <Fragment>
+      {expansionData.map(val => (
+        <ExpansionPanel className={classes.filterExpansionPanel} key={val.title}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+            <Typography className={classes.filtersHeadingTitle}>{val.title}</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails className={classes.filterDetails}>
+            {val.items.map(item => (
+              <div className={classes.formCntrlGrup} key={item.title}>
+                <FormControlLabel
+                  control={<Checkbox checked={true} value="textAnalysis" />}
+                  label={item.title}
+                  classes={{
+                    label: classes.checkboxLabel,
+                  }}
+                />
+                <span className={classes.count}>{item.count}</span>
+              </div>
+            ))}
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      ))}
+    </Fragment>
+  );
+};
 
 export default withStyles(useStyles)(StyledExpansionPanel);
