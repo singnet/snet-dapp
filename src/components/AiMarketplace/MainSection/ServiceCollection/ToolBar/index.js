17520d8a022be8b992b998cb4f511d4745750be0
Vivek205 <n.vin95@gmail.com>
2019-07-08 11:47:08 +0530

    extracting Sort into separate <>

--- src/components/AiMarketplace/MainSection/ServiceCollection/ToolBar/index.js
+++ src/components/AiMarketplace/MainSection/ServiceCollection/ToolBar/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 
-import StyledDropdown from "../../../../common/StyledDropdown";
 import { useStyles } from "./styles";
 import SearchInputToggler from "./SearchInputToggler";
 import ViewToggler from "./ViewToggler";
+import Sort from "./Sort";
 
 const ToolBar = ({ listView, total_count, handleSearchChange, toggleView }) => {
   const [showSearchInput, toggleSearchInput] = useState(false);
@@ -24,8 +24,7 @@ const ToolBar = ({ listView, total_count, handleSearchChange, toggleView }) => {
   return (
     <Grid container spacing={24} className={classes.toolBar}>
       <Grid item xs={12} sm={6} md={6} lg={6} className={classes.sortBySection}>
-        <span className={classes.sortbyTxt}>Sort by:</span>
-        <StyledDropdown labelTxt="" />
+        <Sort />
       </Grid>
       <Grid item xs={12} sm={6} md={6} lg={6} className={classes.iconsContainer}>
         <span className={classes.servicesCount}>{total_count} services &nbsp;&nbsp;&nbsp; | </span>
