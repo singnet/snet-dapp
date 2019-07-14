2bf71b82bf7f55fc1e5c2ad6663f8479c0e94cc9
Vivek205 <n.vin95@gmail.com>
2019-07-14 20:38:26 +0530

    converted MobileHeader into a functional component

--- src/components/common/Header/MobileHeader/index.js
+++ src/components/common/Header/MobileHeader/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import { withStyles } from "@material-ui/styles";
 import Icon from "@material-ui/core/Icon";
 import clsx from "clsx";
@@ -9,56 +9,51 @@ import HeaderActions from "../HeaderActions";
 import NavItem from "../NavItem";
 import { useStyles } from "./styles";
 
-class MobileHeader extends Component {
-  // state = { showMenu: false };
-
-  toggleMobileMenu = () => {
-    // this.setState({ showMenu: !this.state.showMenu });
-    this.props.updateHamburgerState(!this.props.hamburgerMenu);
+const MobileHeader = ({ classes, data, isLoggedIn, hamburgerMenu, updateHamburgerState }) => {
+  const toggleMobileMenu = () => {
+    updateHamburgerState(!hamburgerMenu);
   };
 
-  render() {
-    const { classes, data, isLoggedIn, hamburgerMenu } = this.props;
-    // const { showMenu } = this.state;
-
+  if (!hamburgerMenu) {
     return (
-      <div>
-        <div className={classes.hamburger} onClick={this.toggleMobileMenu}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        {hamburgerMenu ? (
-          <div className={classes.mobileNavContainer}>
-            <div className={classes.closeMenuIcon}>
-              <Icon className={clsx(classes.icon, "fas fa-times")} onClick={this.toggleMobileMenu} />
-            </div>
-            <nav className={classes.mobileNavigation}>
-              <ul>
-                {data.tabs.map(tab => (
-                  <NavItem key={tab.title} title={tab.title} link={tab.link} active={tab.active} />
-                ))}
-                {data.dropdowns.map(dropdown => (
-                  <div className={classes.subMenues}>
-                    <Fragment key={dropdown.label}>
-                      <NavItem title={dropdown.label} subHeader />
-                      {dropdown.list.map(item => (
-                        <NavItem key={item.label} title={item.label} link={dropdown.link} subListItem />
-                      ))}
-                    </Fragment>
-                  </div>
-                ))}
-              </ul>
-              <div className={classes.mobileActionBtns}>
-                <HeaderActions isLoggedIn={isLoggedIn} />
-              </div>
-            </nav>
-          </div>
-        ) : null}
+      <div className={classes.hamburger} onClick={toggleMobileMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     );
   }
-}
+
+  return (
+    <Fragment>
+      <div className={classes.mobileNavContainer}>
+        <div className={classes.closeMenuIcon}>
+          <Icon className={clsx(classes.icon, "fas fa-times")} onClick={toggleMobileMenu} />
+        </div>
+        <nav className={classes.mobileNavigation}>
+          <ul>
+            {data.tabs.map(tab => (
+              <NavItem key={tab.title} title={tab.title} link={tab.link} active={tab.active} />
+            ))}
+            {data.dropdowns.map(dropdown => (
+              <div className={classes.subMenues}>
+                <Fragment key={dropdown.label}>
+                  <NavItem title={dropdown.label} subHeader />
+                  {dropdown.list.map(item => (
+                    <NavItem key={item.label} title={item.label} link={dropdown.link} subListItem />
+                  ))}
+                </Fragment>
+              </div>
+            ))}
+          </ul>
+          <div className={classes.mobileActionBtns}>
+            <HeaderActions isLoggedIn={isLoggedIn} />
+          </div>
+        </nav>
+      </div>
+    </Fragment>
+  );
+};
 
 const mapStateToProps = state => ({
   hamburgerMenu: state.stylesReducer.hamburgerMenu,
