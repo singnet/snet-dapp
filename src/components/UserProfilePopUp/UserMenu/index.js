7319bf2d8aefac032ece02603695894d8521677c
dasari-ananya <ananya.dasari@nirman.io>
2019-07-15 21:40:48 +0530

    temporary removed all tabs except setting and sign out from user profile popup

--- src/components/UserProfilePopUp/UserMenu/index.js
+++ src/components/UserProfilePopUp/UserMenu/index.js
@@ -9,31 +9,31 @@ import { useStyles } from "./styles";
 class UserMenu extends Component {
   state = {
     userMenuItemList: [
-      {
-        menuIcon: "fas fa-user-circle",
-        menuTitle: "Account",
-        menuLink: "/",
-      },
+      // {
+      //   menuIcon: "fas fa-user-circle",
+      //   menuTitle: "Account",
+      //   menuLink: "/",
+      // },
       {
         menuIcon: "fas fa-cog",
         menuTitle: "Settings",
         menuLink: Routes.USER_PROFILE,
       },
-      {
-        menuIcon: "fas fa-project-diagram",
-        menuTitle: "My AI Services",
-        menuLink: "/",
-      },
-      {
-        menuIcon: "fas fa-user-friends",
-        menuTitle: "Teams",
-        menuLink: "/",
-      },
-      {
-        menuIcon: "fas fa-user-circle",
-        menuTitle: "Request Developer's Account",
-        menuLink: "/",
-      },
+      // {
+      //   menuIcon: "fas fa-project-diagram",
+      //   menuTitle: "My AI Services",
+      //   menuLink: "/",
+      // },
+      // {
+      //   menuIcon: "fas fa-user-friends",
+      //   menuTitle: "Teams",
+      //   menuLink: "/",
+      // },
+      // {
+      //   menuIcon: "fas fa-user-circle",
+      //   menuTitle: "Request Developer's Account",
+      //   menuLink: "/",
+      // },
     ],
     userActions: [
       {
