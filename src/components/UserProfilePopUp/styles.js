bf7e74594ce986547f36a378259f4810ac3be651
dasari-ananya <ananya.dasari@nirman.io>
2019-07-16 19:13:22 +0530

    hide the notification icon and dimmed the profile icon but on hover given white color

--- src/components/UserProfilePopUp/styles.js
+++ src/components/UserProfilePopUp/styles.js
@@ -70,10 +70,12 @@ export const useStyles = theme => ({
         marginRight: 18,
         color: theme.palette.text.lightShadedGray,
         fontSize: 19,
+        display: "none",
       },
       "&:last-of-type": {
-        color: theme.palette.text.white,
+        color: theme.palette.text.lightShadedGray,
         fontSize: 35,
+        "&:hover": { color: theme.palette.text.white },
       },
     },
   },
