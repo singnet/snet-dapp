853ca5dac46e0b8b6f0b3a5e43a6ceb15a49ae2d
Vivek205 <n.vin95@gmail.com>
2019-07-17 12:49:20 +0530

    removed Link imports

--- src/components/UserProfile/UserProfileSettings/index.js
+++ src/components/UserProfile/UserProfileSettings/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
