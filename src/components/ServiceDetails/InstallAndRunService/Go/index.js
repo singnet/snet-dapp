78e9d7e7fc95c90fea72e4390fc338aad45f28a5
dasari-ananya <ananya.dasari@nirman.io>
2019-07-19 14:17:22 +0530

    added components

---
+++ src/components/ServiceDetails/InstallAndRunService/Go/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+
+import { useStyles } from "./styles";
+
+const Go = ({ classes }) => {
+  return null;
+};
+
+export default withStyles(useStyles)(Go);
