12f442d8019e71f1291b38e315ffb677789e0828
dasari-ananya <ananya.dasari@nirman.io>
2019-07-22 16:32:10 +0530

    aligned toolbar

--- src/components/AiMarketplace/MainSection/ServiceCollection/ToolBar/styles.js
+++ src/components/AiMarketplace/MainSection/ServiceCollection/ToolBar/styles.js
@@ -1,16 +1,15 @@
 import { makeStyles } from "@material-ui/styles";
 
 export const useStyles = makeStyles(theme => ({
-  serviceCollection: {
-    paddingLeft: 25,
-  },
+  toolBar:{ padding: '10px 0' },
+  serviceCollection: { paddingLeft: 25 },
   sortBySection: {
     display: "flex",
     alignItems: "baseline",
     "& svg": { color: theme.palette.text.primary },
   },
   sortbyTxt: {
-    padding: "0 17px 5px 0",
+    padding: "0 17px 0 0",
     color: theme.palette.text.lightShadedGray,
     fontSize: 16,
   },
@@ -19,16 +18,15 @@ export const useStyles = makeStyles(theme => ({
     fontSize: 16,
   },
   searchBar: {
+    paddingTop: 10,
     "& div": {
       color: theme.palette.text.mediumShadeGray,
-      "&::after": {
-        borderBottomColor: "#9b9b9b !important",
-      },
+      "&::after": { borderBottomColor: "#9b9b9b !important" },
     },
   },
   iconsContainer: {
     display: "flex",
-    alignItems: "flex-end",
+    alignItems: "center",
     justifyContent: "flex-end",
     "& button": {
       border: "none",
--- src/components/common/StyledDropdown/styles.js
+++ src/components/common/StyledDropdown/styles.js
@@ -5,7 +5,7 @@ export const useStyles = makeStyles(theme => ({
     "&:before": { display: "none" },
     "& select": {
       width: "100%",
-      padding: '10px 30px 10px 10px',
+      padding: 'auto',
       '&:hover':{
       	backgroundColor: theme.palette.text.transBlueBorderBgHover
       }
