c1626232d7d98abd7c025fae5910e36973046db9
raamb <raam.bd@gmail.com>
2019-07-22 11:09:03 +0530

    Added example code for Python and JS SDK

--- src/components/ServiceDetails/InstallAndRunService/Python/index.js
+++ src/components/ServiceDetails/InstallAndRunService/Python/index.js
@@ -3,26 +3,42 @@ import { withStyles } from "@material-ui/styles";
 
 import { useStyles } from "./styles";
 import CodeSnippet from "../../../common/CodeSnippet";
-import Function from "../../../common/CodeSnippet/Function";
-import Key from "../../../common/CodeSnippet/Key";
-import ValueString from "../../../common/CodeSnippet/ValueString";
-import ValueNumber from "../../../common/CodeSnippet/ValueNumber";
 
 const Python = ({ classes }) => {
   return (
     <section>
       <h2>Install</h2>
-      <p>Install the Ai service to your Python client wiht pip:</p>
-      <CodeSnippet>
-        snet.
-        <Function text="redirectToDApp" />
-        (&#123; <br />
-        <Key text="items:" />
-        [ <br /> &#47;&#47; Replace with ID of your service <br />
-        &#123; <Key text="id:" />
-        <ValueString text="singularityNet" />,<Key text="maxCalls:" />
-        <ValueNumber text="600" />)
-      </CodeSnippet>
+      <div>
+        Install the SDK using PIP <br />
+        <CodeSnippet>pip install snet.sdk</CodeSnippet>
+      </div>
+      <br />
+      <div>
+        To generate the gRPC client libraries, you need the SingularityNET Command Line Interface, or CLI, which you can
+        install using pip
+        <CodeSnippet>pip install snet-cli</CodeSnippet>
+      </div>
+      <br />
+      <div>
+        Once you have the CLI installed, run the following command to generate gRPC stubs for service <br />
+        <CodeSnippet>snet sdk generate-client-library python &lt;org_id&gt; &lt;service_id&gt;</CodeSnippet>
+      </div>
+      <br />
+      <div>
+        Run the code
+        <CodeSnippet>
+          <div>
+            from snet.sdk import SnetSDK <br />
+            import &lt;stub&gt;_pb2 <br />
+            import &lt;stub&gt;_pb2_grpc <br />
+            from config import config <br /> <br />
+            sdk = SnetSDK(config) <br />
+            service_client = sdk.create_service_client( <br />
+            &lt;org_id&gt;, &lt;service_id&gt;, example_service_pb2_grpc.CalculatorStub) <br />
+            request = example_service_pb2.Numbers(a=20, b=3) <br />
+          </div>
+        </CodeSnippet>
+      </div>
     </section>
   );
 };
