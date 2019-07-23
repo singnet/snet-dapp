47ecf2df9066c9fd416a7e3170a5803c40fb26a7
chetankothari <chetankothari@users.noreply.github.com>
2019-07-23 23:45:58 +0530

    Remove service details out of state

--- src/components/ServiceDetails/index.js
+++ src/components/ServiceDetails/index.js
@@ -10,31 +10,24 @@ import AboutService from "./AboutService";
 import InstallAndRunService from "./InstallAndRunService";
 import { useStyles } from "./styles";
 import { serviceActions } from "../../Redux/actionCreators";
+import { serviceDetails } from '../../Redux/reducers/ServiceReducer';
 
 class ServiceDetails extends Component {
   state = {
-    service_row_id: "",
     activeTab: 0,
   };
 
   componentDidMount = async () => {
     await this.fetchServices();
-    this.populateServiceData();
   };
 
   fetchServices = async () => {
-    const { services, pagination, fetchService } = this.props;
-    if (services && services.length > 0) {
+    const { service, pagination, fetchServices } = this.props;
+    if (service) {
       return;
     }
 
-    return fetchService(pagination);
-  };
-
-  populateServiceData = () => {
-    const { service_row_id } = this.props.match.params;
-    const service = this.props.services.find(service => service.service_row_id === Number(service_row_id));
-    this.setState({ service_row_id, service });
+    return fetchServices(pagination);
   };
 
   handleTabChange = activeTab => {
@@ -42,17 +35,18 @@ class ServiceDetails extends Component {
   };
 
   render() {
-    const { classes } = this.props;
-    const { service, activeTab } = this.state;
+    const { classes, service } = this.props;
+
+    if (!service) {
+      return null;
+    }
+
+    const { activeTab } = this.state;
 
     const tabs = [
       { name: "About", activeIndex: 0, component: <AboutService service={service} /> },
       { name: "Install and Run", activeIndex: 1, component: <InstallAndRunService /> },
     ];
-
-    if (!service) {
-      return null;
-    }
 
     return (
       <Grid container spacing={24} className={classes.serviceDetailContainer}>
@@ -70,19 +64,15 @@ class ServiceDetails extends Component {
   }
 }
 
-ServiceDetails.defaultProps = {
-  services: [],
-};
-
-const mapStateToProps = state => ({
-  services: state.serviceReducer.services,
+const mapStateToProps = (state, ownProps) => ({
+  service: serviceDetails(state, ownProps.match.params.service_row_id),
   pagination: state.serviceReducer.pagination,
   isLoggedIn: state.userReducer.login.isLoggedIn,
   isWalletAssigned: state.userReducer.isWalletAssigned,
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchService: pagination => dispatch(serviceActions.fetchService(pagination)),
+  fetchServices: pagination => dispatch(serviceActions.fetchService(pagination)),
 });
 
 export default connect(
