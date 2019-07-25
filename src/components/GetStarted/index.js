7254ba18fc5c3296f8476b3e4f6a144a5d11002d
dasari-ananya <ananya.dasari@nirman.io>
2019-07-25 18:14:47 +0530

    added get started button part

--- src/components/GetStarted/index.js
+++ src/components/GetStarted/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/styles";
 
+import StyledButton from "../common/StyledButton";
 import { GetStartedCategoriesData } from "../../utility/constants/GetStartedCategories";
 import Category from "./Category";
 import { useStyles } from "./styles";
@@ -13,18 +14,20 @@ const GetStarted = ({ classes }) => {
 				<h2>One Powerful AI Marketplace Solution</h2>
 				<p>The AI Marketplace is a secure and distributed platform for using and building AI applications. This radically compresses time-to-deployment while also democratizing the use of AI throughout your organization.</p>
 			</Grid>
-			{
-				GetStartedCategoriesData.map(item => (
-					<Category 
-						icon={item.categoryIcon} 
-						title={item.categoryTitle} 
-						description={item.categoryDescription} 
-						tabsTitle={item.categoryTabs} 
-						tabsContent={item.categoryTabsContent}
-					/>		
-				))
-			}
-			
+			{GetStartedCategoriesData.map(item => (
+				<Category 
+					icon={item.categoryIcon} 
+					title={item.categoryTitle} 
+					description={item.categoryDescription} 
+					tabsTitle={item.categoryTabs} 
+					tabsContent={item.categoryTabsContent}
+				/>		
+			))}
+			<Grid item xs={12} sm={12} md={12} lg={12} className={classes.SignUpFree}>
+				<h2>Get Started</h2>
+				<p>We've made it fast and easy to experience the AI Marketplace.<br/ >Start with our free account and start demoing our cateloge of AI services in matter of minutes.</p>
+				<StyledButton  btnText="Sign up Free" type="blue"/>
+			</Grid>			
 		</Grid>
 	)
 }
