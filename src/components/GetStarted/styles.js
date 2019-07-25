59754d592060651b0c79fabc131c6ce2f74fa7ee
dasari-ananya <ananya.dasari@nirman.io>
2019-07-25 18:14:47 +0530

    added get started button part

--- src/components/GetStarted/styles.js
+++ src/components/GetStarted/styles.js
@@ -21,5 +21,23 @@ export const useStyles = theme =>({
 			fontSize: 20,
 			lineHeight: '30px'
 		}
+	},
+	SignUpFree:{
+		maxWidth: '47%',
+		margin: '0 auto',
+		textAlign: 'center',
+		'& h2':{
+			color: theme.palette.text.darkShadedGray,
+			fontSize: 32,
+			fontWeight: 600,
+			lineHeight: '43px'
+		},
+		'& p':{
+			margin: '20px 0',
+			color: theme.palette.text.mediumShadeGray,
+			fontFamily: theme.typography.secondary.main,
+			fontSize: 20,
+			lineHeight: '30px'
+		}
 	}
 }) 
