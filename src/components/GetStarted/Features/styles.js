ded04444dab0925588df688cc630492557d2a24a
dasari-ananya <ananya.dasari@nirman.io>
2019-07-26 11:40:21 +0530

    added features

---
+++ src/components/GetStarted/Features/styles.js
@@ -0,0 +1,11 @@
+export const useStyles = theme => ({
+	FeaturesWrapper: { 
+		textAlign: 'center',
+		'& h2':{
+			color: theme.palette.text.darkShadedGray,
+			fontSize: 32,
+			fontWeight: 600,
+			lineHeight: '48px'
+		}, 
+	}
+})
