f23a9113b7f038434396d960a95562f9ce06de54
Vivek205 <n.vin95@gmail.com>
2019-07-30 23:55:33 +0530

    feedback functionality flow done

--- src/components/ServiceDetails/AboutService/ServiceDemo/ThirdPartyAIService.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/ThirdPartyAIService.js
@@ -109,7 +109,6 @@ class ThirdPartyAIService extends Component {
           serviceId={service_id}
           refetchFeedback={this.fetchUserFeedback}
         />
-        <CompletedActions isComplete feedback={feedback} orgId={org_id} serviceId={service_id} />
       </div>
     );
   }
@@ -117,7 +116,7 @@ class ThirdPartyAIService extends Component {
 
 const mapStateToProps = state => ({
   grpcResponse: state.serviceReducer.serviceMethodExecution.response,
-  isComplete: state.serviceReducer.serviceMethodExecution.isComplete,
+  // isComplete: state.serviceReducer.serviceMethodExecution.isComplete,
   username: state.userReducer.username,
 });
 const mapDispatchToProps = dispatch => ({
