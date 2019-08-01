dd305bb5ac80712f3f4b68769f1597c9cc1a3f04
Vivek205 <n.vin95@gmail.com>
2019-08-01 14:11:49 +0530

    separate containers for separate media types

---
+++ src/components/GetStarted/Category/FeatureMedia/GifContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GifContainer = () => {
+  return <div />;
+};
+
+export default GifContainer;
---
+++ src/components/GetStarted/Category/FeatureMedia/ImgContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImgContainer = () => {
+  return <div />;
+};
+
+export default ImgContainer;
