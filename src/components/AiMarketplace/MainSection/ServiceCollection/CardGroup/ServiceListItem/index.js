ebaffcf37bfc42688e932f572e8083590431366d
Vivek205 <n.vin95@gmail.com>
2019-08-12 14:24:29 +0530

    Subheader

--- src/components/AiMarketplace/MainSection/ServiceCollection/CardGroup/ServiceListItem/index.js
+++ src/components/AiMarketplace/MainSection/ServiceCollection/CardGroup/ServiceListItem/index.js
@@ -5,26 +5,24 @@ import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
-import Avatar from '@material-ui/core/Avatar';
+import Avatar from "@material-ui/core/Avatar";
 
 import StyledButton from "../../../../../common/StyledButton";
 import { useStyles } from "./styles";
 import RatingsCount from "../../../../../common/RatingsCount";
+import Subheader from "./Subheader";
 
 const ServiceListItem = props => {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
       <Grid container spacing={24} className={classes.cardItemsContainer}>
-
         <Grid item xs={12} sm={1} md={1} lg={1} className={classes.mediaContainer}>
           <CardMedia className={classes.CardMedia} image={props.cardMedia} title={props.title} />
         </Grid>
 
         <Grid item xs={12} sm={10} md={10} lg={10}>
-
           <CardHeader
             className={classes.cardHeader}
             avatar={
@@ -34,24 +32,17 @@ const ServiceListItem = props => {
             }
             classes={{ title: classes.cardTitle, subheader: classes.cardSubheader }}
             title={props.cardTitle}
-            subheader={props.cardSubheader}
-            // component={
-            //   <div className={classes.ratingSection}>
-            //   <StarRatingComponent
-            //     name="rate1"
-            //     starCount={5}
-            //     value={Number(props.ratingGiven)}
-            //     className={classes.ratingStars}
-            //   />
-            //   <RatingsCount ratingGiven={props.ratingGiven} totalRating={props.totalRating} />
-            // </div>
-            // }
-            action={
-              <StyledButton type="transparent" btnText="demo" />
+            subheader={
+              <Subheader
+                cardSubheader={props.cardSubheader}
+                ratingGiven={props.ratingGiven}
+                totalRating={props.totalRating}
+              />
             }
+            action={<StyledButton type="transparent" btnText="demo" />}
           />
 
-          <CardContent className={classes.cardContent}>            
+          <CardContent className={classes.cardContent}>
             <Typography className={classes.cardTypograpy} component="p">
               {props.cardDescription}
             </Typography>
@@ -65,7 +56,6 @@ const ServiceListItem = props => {
               <RatingsCount ratingGiven={props.ratingGiven} totalRating={props.totalRating} />
             </div>
           </CardContent>
-
         </Grid>
       </Grid>
     </Card>
