122f9e88fea9fb9e0d5e4d87e04ec5c9dcd240d7
Vivek205 <n.vin95@gmail.com>
2019-08-13 16:25:16 +0530

    handleSubmit -> TermsofUse

--- src/components/Onboarding/TermsOfUse/index.js
+++ src/components/Onboarding/TermsOfUse/index.js
@@ -2,23 +2,42 @@ import React, { Component } from "react";
 import { withStyles } from "@material-ui/styles";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
 import StyledButton from "../../common/StyledButton";
 import { useStyles } from "./styles";
 import PrivacyTerms from "./PrivacyTerms";
+import { userActions } from "../../../Redux/actionCreators";
+import Routes from "../../../utility/constants/Routes";
 
 class TermsOfUse extends Component {
   state = {
-    hasAcceptedTerms: false,
+    isTermsAccepted: false,
   };
 
-  handleAcceptTerms = event => {
-    this.setState({ hasAcceptedTerms: event.target.checked });
+  componentDidMount = () => {
+    this.setState({ isTermsAccepted: this.props.isTermsAccepted });
+  };
+
+  handleChange = event => {
+    this.setState({ isTermsAccepted: event.target.checked });
+  };
+
+  handleSubmit = async () => {
+    const { updateUserProfile, history } = this.props;
+    const updatedUserData = { terms_accepted: this.state.isTermsAccepted };
+    await updateUserProfile(updatedUserData);
+    if (history.location.state && history.location.state.sourcePath) {
+      history.push(history.location.state.sourcePath);
+      return;
+    }
+    history.push(`/${Routes.AI_MARKETPLACE}`);
   };
 
   render() {
     const { classes, handleNextSection } = this.props;
-    const { hasAcceptedTerms } = this.state;
+    const { isTermsAccepted } = this.state;
     return (
       <div className={classes.onboardingContainer}>
         <div className={classes.termsOfUseContainer}>
@@ -28,10 +47,10 @@ class TermsOfUse extends Component {
           </div>
           <div className={classes.checkboxAndButton}>
             <FormControlLabel
-              control={<Checkbox checked={hasAcceptedTerms} onChange={this.handleAcceptTerms} color="primary" />}
+              control={<Checkbox checked={isTermsAccepted} onChange={this.handleChange} color="primary" />}
               label="I agree to the Terms of Service"
             />
-            <StyledButton btnText="accept" disabled={!hasAcceptedTerms} onClick={handleNextSection} />
+            <StyledButton btnText="accept" disabled={!isTermsAccepted} onClick={handleNextSection} />
           </div>
         </div>
       </div>
@@ -39,4 +58,15 @@ class TermsOfUse extends Component {
   }
 }
 
-export default withStyles(useStyles)(TermsOfUse);
+const mapStateToProps = state => ({
+  isTermsAccepted: state.userReducer.isTermsAccepted,
+});
+
+const mapDispatchToProps = dispatch => ({
+  updateUserProfile: updatedUserData => dispatch(userActions.updateUserProfile(updatedUserData)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(useStyles)(withRouter(TermsOfUse)));
