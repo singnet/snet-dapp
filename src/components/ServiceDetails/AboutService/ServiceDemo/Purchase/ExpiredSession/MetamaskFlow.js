e73d5b6d0569272ce5133eb45b15a3ab7157c845
Vivek205 <n.vin95@gmail.com>
2019-08-14 19:19:23 +0530

    MetmaskFlow

---
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/MetamaskFlow.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+import { PaymentInfoCardData } from "../../../../../../utility/constants/PurchaseFlow.js";
+import StyledButton from "../../../../../common/StyledButton";
+import PaymentInfoCard from "../PaymentInfoCard";
+import PurchaseDialog from "../PurchaseDialog";
+import ChannelSelectionBox from "../ChannelSelectionBox";
+
+const payTypes = {
+  CHANNEL_BALANCE: "CHANNEL_BALANCE",
+  MULTIPLE_CALLS: "MULTIPLE_CALLS",
+  SINGLE_CALL: "SINGLE_CALL",
+};
+
+export const MetamaskFlow = ({ classes, handleContinue }) => {
+  const [selectedPayType, setSelectedPayType] = useState(payTypes.CHANNEL_BALANCE);
+  const [showPurchaseDialog, setShowPurchaseDialog] = useState(false);
+  const handlePayTypeChange = value => {
+    setSelectedPayType(value);
+  };
+  const handlePurchaseDialogClose = () => {
+    setShowPurchaseDialog(false);
+  };
+  return (
+    <div className={classes.PurchaseFlowContainer}>
+      <PurchaseDialog show={showPurchaseDialog} onClose={handlePurchaseDialogClose} />
+      <p className={classes.PurchaseFlowDescription}>
+        Transfer the style of a “style Image” to a “content image” by choosing them in the boxes below. You can upload a
+        a file from your computer, URL, or select image from the gallery. You can specify additional parameters in the
+        panel below. “Mouse over” for tool tips.
+      </p>
+      <div className={classes.paymentInfoCard}>
+        {PaymentInfoCardData.map(item => (
+          <PaymentInfoCard key={item.title} title={item.title} value={item.value} unit={item.unit} />
+        ))}
+      </div>
+      <div className={classes.ChannelSelectionBoxMainContainer}>
+        <div>
+          <span className={classes.channelSelectionTitle}>Recommended</span>
+          <ChannelSelectionBox
+            title="Channel Balance"
+            description="You have 0.065627 tokens in you channel. This can be used for running demos across all the services from this vendor."
+            checked={selectedPayType === payTypes.CHANNEL_BALANCE}
+            value={payTypes.CHANNEL_BALANCE}
+            onClick={() => handlePayTypeChange(payTypes.CHANNEL_BALANCE)}
+          />
+        </div>
+        <div>
+          <span className={classes.channelSelectionTitle}>Best Value</span>
+          <ChannelSelectionBox
+            title="Multiple Calls"
+            description="Select the no of calls you want to make. The tokens are purchased from the available escrow balance. This  option helps save the gas cost."
+            hasInput
+            checked={selectedPayType === payTypes.MULTIPLE_CALLS}
+            value={payTypes.MULTIPLE_CALLS}
+            onClick={() => handlePayTypeChange(payTypes.MULTIPLE_CALLS)}
+          />
+          <ChannelSelectionBox
+            title="Single Call"
+            description="Tokens are purchsed for a single call. The tokens are purchsed from the available escrow balance."
+            checked={selectedPayType === payTypes.SINGLE_CALL}
+            value={payTypes.SINGLE_CALL}
+            onClick={() => handlePayTypeChange(payTypes.SINGLE_CALL)}
+            hasInput
+            disabled
+          />
+        </div>
+      </div>
+      <div className={classes.buttonContainer}>
+        <StyledButton type="transparent" btnText="Deposit into Escrow" />
+        <StyledButton type="blue" btnText="Continue" onClick={handleContinue} />
+      </div>
+    </div>
+  );
+};
--- src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/index.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/index.js
@@ -1,61 +1,18 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
 
-import { PaymentInfoCardData } from "../../../../../../utility/constants/PurchaseFlow.js";
-
 import AlertBox from "../../../../../common/AlertBox";
 import StyledButton from "../../../../../common/StyledButton";
-import PaymentInfoCard from "../PaymentInfoCard";
-import PurchaseDialog from "../PurchaseDialog";
-import ChannelSelectionBox from "../ChannelSelectionBox";
 import { useStyles } from "./styles";
+import { MetamaskFlow } from "./MetamaskFlow";
 
 const ExpiredSession = ({ classes, handleComplete, metamask }) => {
+  const handleContinue = () => {
+    handleComplete();
+  };
+
   if (metamask) {
-    return (
-      <div className={classes.PurchaseFlowContainer}>
-        <PurchaseDialog />
-        <p className={classes.PurchaseFlowDescription}>
-          Transfer the style of a “style Image” to a “content image” by choosing them in the boxes below. You can upload
-          a a file from your computer, URL, or select image from the gallery. You can specify additional parameters in
-          the panel below. “Mouse over” for tool tips.
-        </p>
-        <div className={classes.paymentInfoCard}>
-          {PaymentInfoCardData.map(item => (
-            <PaymentInfoCard key={item.title} title={item.title} value={item.value} unit={item.unit} />
-          ))}
-        </div>
-        <div className={classes.ChannelSelectionBoxMainContainer}>
-          <div>
-            <span className={classes.channelSelectionTitle}>Recommended</span>
-            <ChannelSelectionBox
-              title="Channel Balance"
-              description="You have 0.065627 tokens in you channel. This can be used for running demos across all the services from this vendor."
-              checked
-            />
-          </div>
-          <div>
-            <span className={classes.channelSelectionTitle}>Best Value</span>
-            <ChannelSelectionBox
-              title="Multiple Calls"
-              description="Select the no of calls you want to make. The tokens are purchased from the available escrow balance. This  option helps save the gas cost."
-              hasInput
-            />
-            <ChannelSelectionBox
-              title="Single Call"
-              description="Tokens are purchsed for a single call. The tokens are purchsed from the available escrow balance."
-              checked={false}
-              hasInput
-              disabled
-            />
-          </div>
-        </div>
-        <div className={classes.buttonContainer}>
-          <StyledButton type="transparent" btnText="Deposit into Escrow" />
-          <StyledButton type="blue" btnText="Continue" />
-        </div>
-      </div>
-    );
+    return <MetamaskFlow handleContinue={handleContinue} classes={classes} />;
   }
   return (
     <div className={classes.ExpiredSessionContainer}>
@@ -64,7 +21,7 @@ const ExpiredSession = ({ classes, handleComplete, metamask }) => {
         message="You have used all your free quota for this service.  Please add a payment method to continue using this service. To know more about adding credits to your acount "
         link="Click here"
       />
-      <StyledButton type="blue" btnText="add payment" onClick={handleComplete} />
+      <StyledButton type="blue" btnText="add payment" onClick={handleContinue} />
     </div>
   );
 };
