5b77b394719b66b47ae6066f2424231a819b88f9
dasari-ananya <ananya.dasari@nirman.io>
2019-08-16 13:23:58 +0530

    added dummy video and gif file to getStarted

--- src/components/GetStarted/Category/FeatureMedia/GifContainer.js
+++ src/components/GetStarted/Category/FeatureMedia/GifContainer.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { withStyles } from "@material-ui/styles";
 
-const GifContainer = ({ content }) => {
-  return <div>{content}</div>;
+import { useStyles } from "./styles";
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+
+const GifContainer = ({ classes, content }) => {
+  return (
+  	<Card className={classes.GifContainer, classes.CardContainer}>
+  		<CardMedia 
+      	component="iframe"
+      	image="https://media.giphy.com/media/pLLLNpJWp6jjW/giphy.gif"
+      	title="Contemplative Reptile" 
+   		/>
+   </Card>
+  )
 };
 
-export default GifContainer;
+export default withStyles(useStyles)(GifContainer);
--- src/components/GetStarted/Category/FeatureMedia/VideoContainer.js
+++ src/components/GetStarted/Category/FeatureMedia/VideoContainer.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { withStyles } from "@material-ui/styles";
 
-import DummyGetStarted from '../../../../assets/images/DummyGetStarted.png';
+import { useStyles } from "./styles";
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
 
-const VideoContainer = ({ content }) => {
-  return <img src={DummyGetStarted} alt="DummyImage" />
+const VideoContainer = ({ classes, content }) => {
+  return (
+  	<Card className={classes.VideoContainer, classes.CardContainer}>
+  		<CardMedia 
+      	component="iframe"
+      	image="http://techslides.com/demos/sample-videos/small.mp4"
+      	title="Contemplative Reptile" 
+      	clasName={classes.CardMediaContainer}
+   		/>
+   </Card>
+  )
 };
 
-export default VideoContainer;
+export default withStyles(useStyles)(VideoContainer);
