fdce51b6f53b2960a372e44c0d1a5fc7e5d865bb
dasari-ananya <ananya.dasari@nirman.io>
2019-08-20 13:36:12 +0530

    spacing adjusted between the service name and provider in list view

--- src/components/AiMarketplace/MainSection/ServiceCollection/CardGroup/ServiceListItem/styles.js
+++ src/components/AiMarketplace/MainSection/ServiceCollection/CardGroup/ServiceListItem/styles.js
@@ -28,7 +28,7 @@ export const useStyles = makeStyles(theme => ({
     height: 120,
   },
   cardHeader: {
-    padding: "6px 10px 0",
+    padding: "0 10px",
     alignItems: "center",
     textAlign: "left",
     "& button": {
@@ -44,7 +44,7 @@ export const useStyles = makeStyles(theme => ({
     color: theme.palette.text.darkShadedGray,
     fontSize: 20,
     letterSpacing: 0.25,
-    padding: "0px 9px 4px 0px",
+    padding: "0 9px 0 0px",
     textAlign: "left",
     fontFamily: theme.typography.primary.main,
     "& h4": { margin: "0 76px 0 0" },
