86533b10501afa1bcb761bcb176ec1370085bfed
Vivek205 <n.vin95@gmail.com>
2019-08-20 18:08:53 +0530

    dynamic channel balance in box

--- src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/MetamaskFlow.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/MetamaskFlow.js
@@ -59,12 +59,12 @@ class MetamaskFlow extends Component {
     },
     {
       title: "Escrow Balance",
-      value: "1.065627",
+      value: "",
       unit: "AGI",
     },
     {
       title: "Channel Balance",
-      value: "0.065627",
+      value: "",
       unit: "AGI",
     },
   ];
@@ -143,6 +143,10 @@ class MetamaskFlow extends Component {
     }
   };
 
+  parseChannelBalFromPaymentCard = () => {
+    return this.PaymentInfoCardData.find(el => el.title === "Channel Balance")[0].value;
+  };
+
   render() {
     const { classes } = this.props;
     const {
@@ -185,7 +189,7 @@ class MetamaskFlow extends Component {
             <span className={classes.channelSelectionTitle}>Recommended</span>
             <ChannelSelectionBox
               title="Channel Balance"
-              description="You have 0.065627 tokens in you channel. This can be used for running demos across all the services from this vendor."
+              description={`You have ${this.parseChannelBalFromPaymentCard()} AGI in you channel. This can be used for running demos across all the services from this vendor.`}
               checked={selectedPayType === payTypes.CHANNEL_BALANCE}
               value={payTypes.CHANNEL_BALANCE}
               onClick={() => this.handlePayTypeChange(payTypes.CHANNEL_BALANCE)}
