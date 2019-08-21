f259965cf3958d0850127306d05d21ce1e468c4c
Vivek205 <n.vin95@gmail.com>
2019-08-21 19:12:34 +0530

    stopLoader on successfull fetchMetering

--- src/Redux/actionCreators/ServiceDetailsActions.js
+++ src/Redux/actionCreators/ServiceDetailsActions.js
@@ -47,7 +47,11 @@ const fetchMeteringDataError = error => dispatch => {
 
 const fetchMeteringDataSuccess = usageData => dispatch => {
   const freeCallsRemaining = usageData.free_calls_allowed - usageData.total_calls_made;
-  dispatch({ type: UPDATE_FREE_CALLS_INFO, payload: { allowed: usageData.free_calls_allowed, remaining: freeCallsRemaining }});
+  dispatch({
+    type: UPDATE_FREE_CALLS_INFO,
+    payload: { allowed: usageData.free_calls_allowed, remaining: freeCallsRemaining },
+  });
+  dispatch(loaderActions.stopAppLoader);
 };
 
 const meteringAPI = (token, orgId, serviceId, userId) => {
