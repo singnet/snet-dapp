3fea61d51ab4b07006a7bc8148f2c394b5ea9fb5
Vivek205 <n.vin95@gmail.com>
2019-08-23 15:07:46 +0530

    Dynamic Alert message

--- src/components/common/NetworkChangeOverlay/index.js
+++ src/components/common/NetworkChangeOverlay/index.js
@@ -7,16 +7,28 @@ import Modal from "@material-ui/core/Modal";
 import Divider from "@material-ui/core/Divider";
 import { withStyles } from "@material-ui/styles";
 
-import AlertBox from "../AlertBox";
+import AlertBox, { alertTypes } from "../AlertBox";
 import { useStyles } from "./styles";
 import { walletTypes } from "../../../Redux/actionCreators/UserActions";
+
+const accountChangeAlert = {
+  header: "Incorrect Metamask Account",
+  type: alertTypes.WARNING,
+  message: `Kindly check the Address which you have set on Metamask. 
+  Please switch it to the registered address to continue using the services.`,
+};
+
+const networkChangeAlert = {
+  header: "Incorrect Metamask Network",
+  type: alertTypes.WARNING,
+  message: `Kindly check the network which you have set on Metamask. 
+  Please switch it to Mainnet to continue using the services.`,
+};
 
 class NetworkChangeOverlay extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      invalidMetaMaskDetails: this.showMetaMaskConfigMismatchOverlay(),
-    };
+    this.state = { alert: {}, ...this.showMetaMaskConfigMismatchOverlay() };
   }
 
   componentDidMount() {
@@ -42,11 +54,11 @@ class NetworkChangeOverlay extends Component {
 
     const sameNetwork = web3Provider.networkVersion === process.env.REACT_APP_ETH_NETWORK;
     if (!sameNetwork) {
-      return true;
+      return { invalidMetaMaskDetails: true, alert: networkChangeAlert };
     }
 
     const sameAddress = web3Provider.selectedAddress.toLowerCase() === wallet.address.toLowerCase();
-    return !sameAddress;
+    return { invalidMetaMaskDetails: !sameAddress, alert: accountChangeAlert };
   };
 
   handleMetaMaskAccountChange = event => {
@@ -59,7 +71,8 @@ class NetworkChangeOverlay extends Component {
       detail: { address },
     } = event;
     const sameAddress = address.toLowerCase() === currentAddress.toLowerCase();
-    this.setState({ invalidMetaMaskDetails: !sameAddress });
+
+    this.setState({ invalidMetaMaskDetails: !sameAddress, alert: accountChangeAlert });
   };
 
   handleMetaMaskNetworkChange = ({ detail: { network } }) => {
@@ -68,21 +81,19 @@ class NetworkChangeOverlay extends Component {
       return;
     }
     const sameNetwork = network === process.env.REACT_APP_ETH_NETWORK;
-    this.setState({ invalidMetaMaskDetails: !sameNetwork });
+    this.setState({ invalidMetaMaskDetails: !sameNetwork, alert: networkChangeAlert });
   };
 
   render() {
     const { classes } = this.props;
+    const { alert, invalidMetaMaskDetails } = this.state;
     return (
-      <Modal disableBackdropClick open={this.state.invalidMetaMaskDetails}>
+      <Modal disableBackdropClick open={invalidMetaMaskDetails}>
         <Card className={classes.card}>
-          <CardHeader title={<h4>Incorrect Metamask Network</h4>} />
+          <CardHeader title={<h4>{alert.header}</h4>} />
           <Divider />
           <CardContent>
-            <AlertBox
-              type="warning"
-              message="Kindly check the channel which you have set on Metamask. Please switch it to Mainnet to continue using the services."
-            />
+            <AlertBox type={alert.type} message={alert.message} />
           </CardContent>
         </Card>
       </Modal>
