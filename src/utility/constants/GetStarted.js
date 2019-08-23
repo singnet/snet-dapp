6b5a369cffd78f8fbf0e77ac3ddc333565d4f57b
dasari-ananya <ananya.dasari@nirman.io>
2019-08-23 13:51:50 +0530

    added content for get started page

--- src/utility/constants/GetStarted.js
+++ src/utility/constants/GetStarted.js
@@ -3,21 +3,50 @@ import DescriptionIcon from "@material-ui/icons/Description";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import ThumbsUpDownIcon from "@material-ui/icons/ThumbsUpDown";
 import SettingsIcon from "@material-ui/icons/Settings";
-
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+
+import CardorListViews from "../../assets/images/GetStarted/BrowsingTheMarketplace/CardorListViews.png";
+import ChangeFilterOptions from "../../assets/images/GetStarted/BrowsingTheMarketplace/ChangeFilterOptions.png";
+import SortOptions from "../../assets/images/GetStarted/BrowsingTheMarketplace/SortOptions.png";
+import RatingandRanking from "../../assets/images/GetStarted/BrowsingTheMarketplace/RatingandRanking.png";
+import Search from "../../assets/images/GetStarted/BrowsingTheMarketplace/Search.png";
+
+import AboutDetails from "../../assets/images/GetStarted/ChooseingYourAi/AboutDetails.png";
+import Pricing from "../../assets/images/GetStarted/ChooseingYourAi/Pricing.png";
+import DemoTrial from "../../assets/images/GetStarted/ChooseingYourAi/DemoTrial.png";
+import InstallRun from "../../assets/images/GetStarted/ChooseingYourAi/Install&Run.png";
+import Tutorials from "../../assets/images/GetStarted/ChooseingYourAi/Tutorials.png";
+
+import AccessingtheDemo from "../../assets/images/GetStarted/DemoAiServices/AccessingtheDemo.png";
+import ConfigureAIParameters from "../../assets/images/GetStarted/DemoAiServices/ConfigureAIParameters.png";
+import Metamask from "../../assets/images/GetStarted/DemoAiServices/Metamask.png";
+import Seeoutputresults from "../../assets/images/GetStarted/DemoAiServices/Seeoutputresults.png";
+
+import RatingAIServices from "../../assets/images/GetStarted/RateReviewImprove/RatingAIServices.png";
+import AccessingSortingrReviews from "../../assets/images/GetStarted/RateReviewImprove/AccessingSortingrReviews.png";
+import WritingReview from "../../assets/images/GetStarted/RateReviewImprove/WritingReview.png";
+
+import Install from "../../assets/images/GetStarted/MakeItYourAI/Install.png";
+import Run from "../../assets/images/GetStarted/MakeItYourAI/Run.png";
+import MultipleChannels from "../../assets/images/GetStarted/MakeItYourAI/MultipleChannels.png";
+import Documentation from "../../assets/images/GetStarted/MakeItYourAI/Documentation.png";
+
+import ManagingyourAGItokens from "../../assets/images/GetStarted/UsingAgi/ManagingyourAGItokens.png";
+import Metamaskwalletsupport from "../../assets/images/GetStarted/UsingAgi/Metamaskwalletsupport.png";
+import Usingchannelbalances from "../../assets/images/GetStarted/UsingAgi/Usingchannelbalances.png";
 
 export const GetStartedCategoriesData = [
   {
     categoryIcon: SeacrhIcon,
     categoryTitle: "Browsing the marketplace",
     categoryDescription:
-      "Exploring AI - The SingularityNET AI Marketplace hosts a wide variety of AI services that range from Pneumonia Detection to Image Colorisation. You can view all of them by default as shown here but also search by name, organization and tags.",
+      "Exploring AI - The SingularityNET AI Marketplace hosts a wide variety of AI services that range from Pneumonia Detection to Image Colorisation. You can view all of them by default in “Card view” as shown here but also search by category, rating, most recent and favourites.",
     categoryTabs: [
-      { title: "AI Services list", media: { type: "gif", content: "" } },
-      /*{ title: "Change Filter options", media: { type: "gif", content: "content2" } },
-      { title: "Sort options", media: { type: "gif", content: "content3" } },
-      { title: "Rating Ranking", media: { type: "gif", content: "content4" } },
-      { title: "Feature Label 5", media: { type: "gif", content: "content5" } },*/
+      { title: "Card or List Views ", media: { type: "img", content: CardorListViews } },
+      { title: "Change Filter Options", media: { type: "img", content: ChangeFilterOptions } },
+      { title: "Sort Options", media: { type: "img", content: SortOptions } },
+      { title: "Rating and Rankings", media: { type: "img", content: RatingandRanking } },
+      { title: "Search", media: { type: "img", content: Search } }
     ],
   },
   {
@@ -26,52 +55,59 @@ export const GetStartedCategoriesData = [
     categoryDescription:
       "AI Services - Each AI service on this platform is unique. Click on any service you would like to use to find out more about its required input, expected output, cost, usage, developer’s note and much more!",
     categoryTabs: [
-      { title: "AI Service Details", media: { type: "video", content: "" } },
-      /*{ title: "Feature Label 2", media: { type: "gif", content: "content" } },
-      { title: "Feature Label 3", media: { type: "gif", content: "content" } },
-      { title: "Feature Label 4", media: { type: "gif", content: "content" } },
-      { title: "Feature Label 5", media: { type: "gif", content: "content" } },*/
+      { title: "About Details", media: { type: "img", content: AboutDetails } },
+      { title: "Pricing", media: { type: "img", content: Pricing } },
+      { title: "Demo Trial", media: { type: "img", content: DemoTrial  } },
+      { title: "Install & Run", media: { type: "img", content: InstallRun  } },
+      { title: "Tutorials", media: { type: "img", content: Tutorials } }
     ],
   },
   {
     categoryIcon: PlayCircleFilledIcon,
-    categoryTitle: "Rate, Review, Improve",
+    categoryTitle: "Demo AI Services",
     categoryDescription:
-      "Feedback - Democratizing AI requires collaboration, and not just between developers. Ratings and reviews help developers improve their algorithms as well as the design of their AI service. It’s also an opportunity for new teams to be born. ",
+      "Try it out yourself! – Majority of services have free demo calls that you can try right away.  Input your parameters and data and let AI process your request instantly.  After your trial demo calls used up, you can still run the demos using MetaMask via AGI tokens. ",
     categoryTabs: [
-      { title: "Provide feedback", media: { type: "img", content: "" } },
-      /*{ title: "Change Filter options", media: { type: "gif", content: "content" } },
-      { title: "Sort options", media: { type: "gif", content: "content" } },
-      { title: "Rating Ranking", media: { type: "gif", content: "content" } },
-      { title: "Feature Label 5", media: { type: "gif", content: "content" } },*/
+      { title: "Accessing the demo", media: { type: "img", content: AccessingtheDemo } },
+      { title: "Configure AI parameters", media: { type: "gif", content: ConfigureAIParameters } },
+      { title: "See output results", media: { type: "gif", content: Seeoutputresults } },
+      { title: "Metamask (coming soon)", media: { type: "gif", content: Metamask } }
     ],
   },
   {
     categoryIcon: ThumbsUpDownIcon,
-    categoryTitle: "Using AGI",
+    categoryTitle: "Rate, Review, Improve",
     categoryDescription:
-      "Own your funds - Whether you want to use services, delete your account or use your funds outside of the platform, you are the sole controller of your funds. Your wallet, your cryptographic key, your AGI. ",
+      "Feedback - Democratizing AI requires collaboration, and not just between developers. Ratings and reviews help developers improve their algorithms as well as the design of their AI service. It’s also an opportunity for new teams to be born. ",
     categoryTabs: [
-      { title: "Payment", media: { type: "video", content: "content" } },
-      /*{ title: "Tutorials", media: { type: "gif", content: "content" } },
-      { title: "Reviews", media: { type: "gif", content: "content" } },
-      { title: "Feature Label 4", media: { type: "gif", content: "content" } },
-      { title: "Feature Label 5", media: { type: "gif", content: "content" } },*/
+      { title: "Rating AI Services", media: { type: "img", content: RatingAIServices } },
+      { title: "Accessing & sorting reviews", media: { type: "img", content: AccessingSortingrReviews } },
+      { title: "Writing review ", media: { type: "img", content: WritingReview } }
     ],
   },
   {
     categoryIcon: SettingsIcon,
     categoryTitle: "Make it your AI",
     categoryDescription:
-      "Install & run it - It has never been easier to find and integrate AI algorithms. You can now seamlessly integrate a service to your website, app or other product via the SingularityNET SDK. ",
+      "Install & run it - It has never been easier to find and integrate AI algorithms. Regardless of your internal tech capabilities, you can now seamlessly integrate a service to your website, app or other product via the SingularityNET AI Marketplace. ",
     categoryTabs: [
-      { title: "Using the SDK", media: { type: "video", content: "content" } },
-      /*{ title: "Change Filter options", media: { type: "gif", content: "content" } },
-      { title: "Sort options", media: { type: "gif", content: "content" } },
-      { title: "Rating Ranking", media: { type: "gif", content: "content" } },
-      { title: "Feature Label 5", media: { type: "gif", content: "content" } },*/
+      { title: "Install", media: { type: "img", content: Install } },
+      { title: "Run", media: { type: "img", content: Run } },
+      { title: "Multiple languages", media: { type: "img", content: MultipleChannels } },
+      { title: "documentation", media: { type: "img", content: Documentation } }
     ],
   },
+  {
+    categoryIcon: SettingsIcon,
+    categoryTitle: "Using AGI",
+    categoryDescription:
+      "Own your funds - Whether you want to use services, delete your account or use your funds outside of the platform, you are the sole controller of your funds. Your wallet, your cryptographic key, your AGI. ",
+    categoryTabs: [
+      { title: "Managing your AGI tokens", media: { type: "img", content: ManagingyourAGItokens  } },
+      { title: "Metamask wallet support", media: { type: "img", content: Metamaskwalletsupport } },
+      { title: "Using channel balances", media: { type: "img", content: Usingchannelbalances } }
+    ],
+  }
 ];
 
 export const GetStartedFeaturesData = [
