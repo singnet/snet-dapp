f531a61c071aa4f0372afd5bb7b1a06ab9b1780c
dasari-ananya <ananya.dasari@nirman.io>
2019-08-26 14:59:00 +0530

    alignmnet adjustment in install and run tab

--- src/components/ServiceDetails/ProjectDetails/styles.js
+++ src/components/ServiceDetails/ProjectDetails/styles.js
@@ -3,7 +3,6 @@ export const useStyles = theme => ({
     boxShadow: "0 1px 1px 0 rgba(0,0,0,0.07), 0 2px 1px -1px rgba(0,0,0,0.07), 0 1px 3px 0 rgba(0,0,0,0.1)",
     padding: "0 25px 30px 0",
     borderRadius: 4,
-    marginTop: 30,
     backgroundColor: theme.palette.text.white,
     "& h3": { marginBottom: 15 },
     "& > div": {
