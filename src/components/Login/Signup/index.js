617112141254862b235d4a040704996b9887f65b
dasari-ananya <ananya.dasari@nirman.io>
2019-08-28 18:48:36 +0530

    removed unneccesary defination

--- src/components/Login/Signup/index.js
+++ src/components/Login/Signup/index.js
@@ -19,7 +19,7 @@ class SignUp extends Component {
     nickname: "",
     email: "",
     password: "",
-    alert: { type: undefined, message: undefined },
+    alert: {},
     toBeConfirmed: false,
     otp: "",
   };
