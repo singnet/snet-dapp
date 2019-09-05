897848c500b455886b167ba154f0ec5aa3ebfc6e
dasari-ananya <ananya.dasari@nirman.io>
2019-09-05 12:55:20 +0530

    removed unwanted padding

--- src/components/HOC/WithInAppHeader/styles.js
+++ src/components/HOC/WithInAppHeader/styles.js
@@ -2,6 +2,9 @@ import { makeStyles } from "@material-ui/styles";
 
 export const useStyles = makeStyles(theme => ({
   scrollableContent: {
+  	height: 'calc(100vh - 70px)',
     position: "absolute",
+    top: 70,
+    overflowY: 'scroll'
   },
 }));
