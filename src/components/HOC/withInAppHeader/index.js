f93c1f463c640b3b3cbd0f6d7061c5cc5ee687ad
Vivek <n.vin95@gmail.com>
2019-09-05 12:46:15 +0530

    styles for withInApp header

--- src/components/HOC/withInAppHeader/index.js
+++ src/components/HOC/withInAppHeader/index.js
@@ -2,15 +2,17 @@ import React, { Fragment } from "react";
 
 import Header from "../common/Header";
 import Footer from "../common/Footer";
+import { useStyles } from "./styles";
 
 const withInAppWrapper = InputComponent => {
   return props => {
+    const classes = useStyles();
     return (
       <Fragment>
         <Header />
-        <div className="scrollableContent">
-        	<InputComponent {...props} />
-        	<Footer />
+        <div className={classes.scrollableContent}>
+          <InputComponent {...props} />
+          <Footer />
         </div>
       </Fragment>
     );
---
+++ src/components/HOC/withInAppHeader/styles.js
@@ -0,0 +1,7 @@
+import { makeStyles } from "@material-ui/styles";
+
+export const useStyles = makeStyles(theme => ({
+  scrollableContent: {
+    position: "absolute",
+  },
+}));
