f8c9b36b5a71cb24f23d8269716a9b908ccacaf1
Vivek <n.vin95@gmail.com>
2019-09-06 12:06:29 +0530

    fixed withStyles Import

--- src/components/common/StyledMenu/index.js
+++ src/components/common/StyledMenu/index.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from "react";
 import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/styles";
 import CaretIcon from "@material-ui/icons/ArrowDropDown";
 
 import { useStyles } from "./styles";
