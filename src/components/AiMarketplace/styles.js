17b49fa0f9529b712c5e09871e827425be56ea39
dasari-ananya <ananya.dasari@nirman.io>
2019-09-09 12:13:32 +0530

    service list page responsive styling for S screen

--- src/components/AiMarketplace/styles.js
+++ src/components/AiMarketplace/styles.js
@@ -6,13 +6,13 @@ export const useStyles = theme => ({
     "@media(max-width: 1279px)": { width: "98%" },
   },
   topSectionCotainer: {
-    "@media(max-width: 1023px)": {
+    "@media(max-width: 1024px)": {
       flexDirection: "column",
       alignItems: "center",
     },
   },
   titleContainer: {
-    "@media(max-width: 1023px)": { maxWidth: "100%" },
+    "@media(max-width: 1024px)": { maxWidth: "100%" },
   },
   title: {
     margin: "33px 0 0 0",
@@ -20,11 +20,11 @@ export const useStyles = theme => ({
     fontSize: 32,
     lineHeight: "57px",
     "@media(max-width: 1279px)": { marginTop: 16 },
-    "@media(max-width: 1023px)": { fontSize: 28 },
+    "@media(max-width: 1024px)": { fontSize: 28 },
     "@media(max-width: 768px)": { marginTop: 0 },
   },
   descriptionContainer: {
-    "@media(max-width: 1023px)": {
+    "@media(max-width: 1024px)": {
       maxWidth: "100%",
       textAlign: "center  ",
     },
@@ -44,7 +44,7 @@ export const useStyles = theme => ({
       paddingRight: 0,
       paddingTop: 16
     },
-    "@media(max-width: 1023px)": { paddingTop: 0 },
+    "@media(max-width: 1024px)": { paddingTop: 0 },
   },
   signupLink: {
     textDecoration: "none",
