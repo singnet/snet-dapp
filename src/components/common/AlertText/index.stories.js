31b1dbda74e4131ed537f1c3fa299e38f67ea410
Vivek <n.vin95@gmail.com>
2019-09-13 14:52:43 +0530

    Stories of AlertText

--- src/components/common/AlertText/index.stories.js
+++ src/components/common/AlertText/index.stories.js
@@ -5,5 +5,8 @@ import AlertText from "./";
 import { alertTypes } from "../AlertBox";
 
 storiesOf("AlertText")
-  .add("_default", () => <AlertText />)
-  .add("Success", () => <AlertText type={alertTypes.SUCCESS} message="Success alert !" />);
+  .add("_default", () => <AlertText message="default" />)
+  .add("Success", () => <AlertText type={alertTypes.SUCCESS} message="Success alert !" />)
+  .add("Info", () => <AlertText type={alertTypes.INFO} message="Success alert !" />)
+  .add("Warning", () => <AlertText type={alertTypes.WARNING} message="Success alert !" />)
+  .add("Error", () => <AlertText type={alertTypes.ERROR} message="Success alert !" />);
