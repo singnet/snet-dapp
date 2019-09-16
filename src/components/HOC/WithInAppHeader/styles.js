aa61010d82b0f05e9f251e7488fc258447c191a4
Vivek <n.vin95@gmail.com>
2019-09-16 11:37:23 +0530

    typo

--- src/components/HOC/WithInAppHeader/styles.js
+++ src/components/HOC/WithInAppHeader/styles.js
@@ -7,6 +7,6 @@ export const useStyles = makeStyles(theme => ({
     position: "absolute",
     top: 70,
     overflowY: "scroll",
-    backgroundColor: 'theme.palette.text.offWhiteColor'
+    backgroundColor: theme.palette.text.offWhiteColor,
   },
 }));
