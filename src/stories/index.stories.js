28df57df3dc2e54d027192561c3b83047385ed73
Vivek <n.vin95@gmail.com>
2019-09-16 17:31:17 +0530

    removed unused addons / imports

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,26 +1,10 @@
 import React from "react";
-
-import { storiesOf, addDecorator } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
+import { addDecorator } from "@storybook/react";
 import { ThemeProvider } from "@material-ui/styles";
 
-import { Button, Welcome } from "@storybook/react/demo";
 import theme from "../assets/Theme";
 
 // Global Configs
 
 const MUIDecorator = story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>;
 addDecorator(MUIDecorator);
-
-storiesOf("Welcome", module).add("to Storybook", () => <Welcome showApp={linkTo("Button")} />);
-
-storiesOf("Button", module)
-  .add("with text", () => <Button onClick={action("clicked")}>Hello Button</Button>)
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
