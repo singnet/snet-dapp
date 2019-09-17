da95c309265e3dcbdad0ae3e7cf631718fa23502
Vivek <n.vin95@gmail.com>
2019-09-17 15:06:56 +0530

    removed unused imports and comments

--- src/stories/SNETImageUpload.stories.js
+++ src/stories/SNETImageUpload.stories.js
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 import { storiesOf } from "@storybook/react";
 import SNETImageUpload from "../assets/thirdPartyServices/standardComponents/SNETImageUpload";
 
-let imagePath;
-const setImagePath = data => {
-  imagePath = data;
-};
+const setImagePath = data => data;
 
 const WithReqdProps = props => <SNETImageUpload imageDataFunc={setImagePath} {...props} />;
 
@@ -22,8 +19,8 @@ storiesOf("SNETImageUpload")
   .add("Custom Image Tab Width", () => <WithReqdProps width="1000px" />)
   .add("Custom Image Tab height", () => <WithReqdProps tabHeight={1000} />)
   .add("Max Image Size(mb)", () => <WithReqdProps maxImageSize={1000000} />)
-  .add("Max Image Width", () => <WithReqdProps maxImageWidth={100} />)
-  .add("Max Image Height", () => <WithReqdProps maxImageHeight={100} />)
+  .add("Max Image Width", () => <WithReqdProps maxImageWidth={10} />)
+  .add("Max Image Height", () => <WithReqdProps maxImageHeight={10} />)
   .add("Display Proportional Image", () => <WithReqdProps maxImageHeight={100} />)
   .add("Output image", () => <SNETOutputImage outputImageName="Custom Output name" />)
   .add("Output Title", () => <SNETOutputImage outputImageName="Custom Output name" displayModeTitle="Custom Title" />)
@@ -37,46 +34,3 @@ storiesOf("SNETImageUpload")
   .add("Custom Comparision Title", () => (
     <SNETOutputImage outputImageName="Custom Output name" comparisonTabTitle="Custom" />
   ));
-
-// SNETImageUpload.propTypes = {
-//   width: PropTypes.string, // e.g.: "500px", "50%" (of parent component width)
-//   tabHeight: PropTypes.number, // a number without units
-//   imageDataFunc: PropTypes.func.isRequired,
-//   imageName: PropTypes.string,
-//   disableUploadTab: PropTypes.bool, // If true disables upload tab
-//   disableUrlTab: PropTypes.bool, // If true disables url tab
-//   disableResetButton: PropTypes.bool, // If true disables image reset button
-//   returnByteArray: PropTypes.bool, // whether to return base64 or byteArray image data
-//   outputFormat: PropTypes.oneOf(["image/png", "image/jpg", "image/jpeg"]),
-//   allowedInputTypes: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-//   maxImageSize: PropTypes.number, // 10 mb
-//   maxImageWidth: PropTypes.number,
-//   maxImageHeight: PropTypes.number,
-//   displayProportionalImage: PropTypes.bool,
-//   allowURL: PropTypes.bool,
-//   instantUrlFetch: PropTypes.bool,
-//   imageGallery: PropTypes.arrayOf(PropTypes.string),
-//   galleryCols: PropTypes.number,
-//   infoTip: PropTypes.string,
-//   mainColor: PropTypes.object,
-
-//   // Output mode props
-//   displayModeTitle: PropTypes.string,
-//   outputImage: PropTypes.string,
-//   outputImageMimeType: PropTypes.oneOf([
-//     "application/octet-stream",
-//     "image/png",
-//     "image/jpg",
-//     "image/jpeg",
-//     "image/gif",
-//   ]),
-//   outputImageName: PropTypes.string,
-//   disableInputTab: PropTypes.bool,
-//   disableOutputTab: PropTypes.bool,
-//   disableComparisonTab: PropTypes.bool,
-//   disableDownloadButton: PropTypes.bool,
-//   overlayInputImage: PropTypes.bool,
-//   inputTabTitle: PropTypes.string,
-//   outputTabTitle: PropTypes.string,
-//   comparisonTabTitle: PropTypes.string,
-// };
