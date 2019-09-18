15dfef0104efdef94fbafa520ac8d661b7a7bb4b
Vivek <n.vin95@gmail.com>
2019-09-18 18:16:42 +0530

    addons

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,5 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
+import 'storybook-addon-jsx/register';
+import 'storybook-addon-react-live-edit/dist/register';
+import 'storybook-addon-react-docgen/register';
--- src/stories/SNETImageUpload.stories.js
+++ src/stories/SNETImageUpload.stories.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { storiesOf } from "@storybook/react";
 import SNETImageUpload from "../assets/thirdPartyServices/standardComponents/SNETImageUpload";
+import withPropsCombinations from "react-storybook-addon-props-combinations";
+import { jsxDecorator } from "storybook-addon-jsx";
+import withLiveEditScope from "storybook-addon-react-live-edit/dist/withLiveEditScope";
 
 const setImagePath = data => data;
 
@@ -24,27 +27,41 @@ const styleGallery = [
 ];
 
 storiesOf("SNETImageUpload", module)
-  .add("_default", () => <WithReqdProps />)
-  .add("Disabled Upload Tab", () => <WithReqdProps disableUploadTab />)
-  .add("Disabled URL", () => <WithReqdProps disableUrlTab />)
-  .add("Image Gallery", () => <WithReqdProps imageGallery={styleGallery} />)
-  .add("Gallery Columns", () => <WithReqdProps imageGallery={styleGallery} galleryCols={5} />)
-  .add("Disabled Reset Button", () => <WithReqdProps disableResetButton />)
-  .add("Custom Image Tab Width", () => <WithReqdProps width="1000px" />)
-  .add("Custom Image Tab height", () => <WithReqdProps tabHeight={1000} />)
-  .add("Max Image Size(mb)", () => <WithReqdProps maxImageSize={1000000} />)
-  .add("Max Image Width", () => <WithReqdProps maxImageWidth={10} />)
-  .add("Max Image Height", () => <WithReqdProps maxImageHeight={10} />)
-  .add("Display Proportional Image", () => <WithReqdProps maxImageHeight={100} />)
-  .add("Output image", () => <SNETOutputImage outputImageName="Custom Output name" />)
-  .add("Output Title", () => <SNETOutputImage outputImageName="Custom Output name" displayModeTitle="Custom Title" />)
-  .add("Disable Input Tab", () => <SNETOutputImage outputImageName="Custom Output name" disableInputTab />)
-  .add("Disable Output Tab", () => <SNETOutputImage outputImageName="Custom Output name" disableOutputTab />)
-  .add("Disable Comparision Tab", () => <SNETOutputImage outputImageName="Custom Output name" disableComparisonTab />)
-  .add("Disable download button", () => <SNETOutputImage outputImageName="Custom Output name" disableDownloadButton />)
-  .add("Overlay Input Image", () => <SNETOutputImage outputImageName="Custom Output name" overlayInputImage />)
-  .add("Custom Input Title", () => <SNETOutputImage outputImageName="Custom Output name" inputTabTitle="Custom" />)
-  .add("Custom Output Title", () => <SNETOutputImage outputImageName="Custom Output name" outputTabTitle="Custom" />)
-  .add("Custom Comparision Title", () => (
-    <SNETOutputImage outputImageName="Custom Output name" comparisonTabTitle="Custom" />
-  ));
+  .addParameters({
+    props: {
+      propTables: [SNETImageUpload],
+    },
+  })
+  .addDecorator(jsxDecorator)
+  .addDecorator(withLiveEditScope({ React, SNETImageUpload, setImagePath }))
+  .addLiveSource("live source", `return <SNETImageUpload imageDataFunc={setImagePath} disableUploadTab />`)
+  .add(
+    "_default",
+    withPropsCombinations(WithReqdProps, {
+      disabledUrlTab: [false, true],
+    })
+  )
+
+  .add("Disabled Upload Tab", () => <SNETImageUpload imageDataFunc={setImagePath} disableUploadTab />);
+// .add("Disabled URL", () => <WithReqdProps disableUrlTab />)
+// .add("Image Gallery", () => <WithReqdProps imageGallery={styleGallery} />)
+// .add("Gallery Columns", () => <WithReqdProps imageGallery={styleGallery} galleryCols={5} />)
+// .add("Disabled Reset Button", () => <WithReqdProps disableResetButton />)
+// .add("Custom Image Tab Width", () => <WithReqdProps width="1000px" />)
+// .add("Custom Image Tab height", () => <WithReqdProps tabHeight={1000} />)
+// .add("Max Image Size(mb)", () => <WithReqdProps maxImageSize={1000000} />)
+// .add("Max Image Width", () => <WithReqdProps maxImageWidth={10} />)
+// .add("Max Image Height", () => <WithReqdProps maxImageHeight={10} />)
+// .add("Display Proportional Image", () => <WithReqdProps maxImageHeight={100} />)
+// .add("Output image", () => <SNETOutputImage outputImageName="Custom Output name" />)
+// .add("Output Title", () => <SNETOutputImage outputImageName="Custom Output name" displayModeTitle="Custom Title" />)
+// .add("Disable Input Tab", () => <SNETOutputImage outputImageName="Custom Output name" disableInputTab />)
+// .add("Disable Output Tab", () => <SNETOutputImage outputImageName="Custom Output name" disableOutputTab />)
+// .add("Disable Comparision Tab", () => <SNETOutputImage outputImageName="Custom Output name" disableComparisonTab />)
+// .add("Disable download button", () => <SNETOutputImage outputImageName="Custom Output name" disableDownloadButton />)
+// .add("Overlay Input Image", () => <SNETOutputImage outputImageName="Custom Output name" overlayInputImage />)
+// .add("Custom Input Title", () => <SNETOutputImage outputImageName="Custom Output name" inputTabTitle="Custom" />)
+// .add("Custom Output Title", () => <SNETOutputImage outputImageName="Custom Output name" outputTabTitle="Custom" />)
+// .add("Custom Comparision Title", () => (
+//   <SNETOutputImage outputImageName="Custom Output name" comparisonTabTitle="Custom" />
+// ));
