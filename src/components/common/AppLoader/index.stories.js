cdaac387f51ffda1cee3a524d16eb7f86e5b1e24
Vivek <n.vin95@gmail.com>
2019-09-23 12:11:14 +0530

    removed unused Providers

--- src/components/common/AppLoader/index.stories.js
+++ src/components/common/AppLoader/index.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import AppLoader from ".";
+import { AppLoader } from "./";
 
 storiesOf("AppLoader", module).add("_default", () => (
   <AppLoader loading loaderHeader="Sample Header" loaderText="Sample Text" />
--- src/stories/Providers.js
+++ src/stories/Providers.js
@@ -1,14 +1,6 @@
 import React from "react";
-import { Router } from "react-router";
-import { Provider } from "react-redux";
 import { ThemeProvider } from "@material-ui/styles";
 
-const ProviderWrapper = ({ children, store, theme, history }) => (
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <Router history={history}>{children}</Router>
-    </ThemeProvider>
-  </Provider>
-);
+const ProviderWrapper = ({ children, theme }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 
 export default ProviderWrapper;
