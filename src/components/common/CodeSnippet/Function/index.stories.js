0415c083012652714fe7f6f831afba1a936ec50a
Vivek <n.vin95@gmail.com>
2019-09-24 15:37:32 +0530

    story - codesnippet - function

---
+++ src/components/common/CodeSnippet/Function/index.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Function from "./";
+
+storiesOf("CodeSnippet | Function", module).add("_default", () => <Function text="sample function" />);
