9fef87b5a47804ead27a7e4a61b890bf2b9d16a2
dasari-ananya <ananya.dasari@nirman.io>
2019-09-24 20:35:25 +0530

    created popgress bar components

---
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/CreateWalletPopup/Details/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+
+import { useStyles } from "./styles";
+
+const Details = ({ classes }) => {
+  return <span>details</span>
+};
+
+export default withStyles(useStyles)(Details);
--- src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/CreateWalletPopup/index.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/CreateWalletPopup/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { withStyles } from "@material-ui/styles";
 import Modal from "@material-ui/core/Modal";
 import Card from "@material-ui/core/Card";
@@ -11,38 +11,67 @@ import CardActions from "@material-ui/core/CardActions";
 import ProgressBar from "../../../../../../../common/ProgressBar";
 import StyledButton from "../../../../../../../common/StyledButton";
 import StyledDropdown from "../../../../../../../common/StyledDropdown";
+import Details from "./Details";
+import Purchase from "./Purchase";
+import PrivateKey from "./PrivateKey";
+import Summary from "./Summary";
+import PopupDetails from "../PopupDetails";
 
 import { useStyles } from "./styles";
 
-const CreateWalletPopup = ({ classes, handleClose, open }) => {
+class CreateWalletPopup extends Component {
+	state = {
+    progressText: ["Details", "Purchase", "Private Key", "Summary"],
+    activeSection: 1
+  }
 
-	const handleCancel = () => {
-    handleClose();
+	handleCancel = () => {
+    this.props.handleClose();
   };
 
-	return (
-		<div className={classes.generalAccWalletContainer}>
-			<Modal open={open} onClose={handleCancel} className={classes.Modal}>
-        <Card className={classes.card}>
-          <CardHeader
-            className={classes.CardHeader}
-            title="Create General Account Wallet"
-            action={
-              <IconButton onClick={handleCancel}>
-                <CloseIcon />
-              </IconButton>
-            }
-          />
-          <CardContent className={classes.CardContent}>
-          </CardContent>
-          <CardActions className={classes.CardActions}>
-            <StyledButton btnText="cancel" type="transparent" />
-            <StyledButton btnText="Continue" type="blue"/>
-          </CardActions>
-        </Card>
-      </Modal>
-		</div>
-	);
+	render(){
+		const { classes, open } = this.props;
+		const { progressText, activeSection } = this.state;
+
+		const PopupProgressBarComponents = [ 
+			{ component: <Details/> },
+			{ component: <Purchase /> },
+			{ component: <PrivateKey /> },
+			{ component: <Summary /> }
+		];
+
+		return (
+			<div className={classes.generalAccWalletContainer}>
+				<Modal open={open} onClose={this.handleCancel} className={classes.Modal}>
+	        <Card className={classes.card}>
+	          <CardHeader
+	            className={classes.CardHeader}
+	            title="Create General Account Wallet"
+	            action={
+	              <IconButton onClick={this.handleCancel}>
+	                <CloseIcon />
+	              </IconButton>
+	            }
+	          />
+	          <CardContent className={classes.CardContent}>
+	          	{PopupProgressBarComponents.map((item, index) => (
+          			<PopupDetails
+          				item={item}
+			            active={activeSection === index + 1}
+			            activeSection={activeSection}
+			            progressText={progressText}
+			          />
+        			))}
+	          </CardContent>
+	          <CardActions className={classes.CardActions}>
+	            <StyledButton btnText="cancel" type="transparent" />
+	            <StyledButton btnText="Continue" type="blue"/>
+	          </CardActions>
+	        </Card>
+	      </Modal>
+			</div>
+		);
+	}	
 }
 
 export default withStyles(useStyles)(CreateWalletPopup);
