8337df0dd7f48d750d49258ca5995c949744b90b
dasari-ananya <ananya.dasari@nirman.io>
2019-09-26 10:15:22 +0530

    private ket tab's content designed

--- src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/CreateWalletPopup/PrivateKey/index.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/CreateWalletPopup/PrivateKey/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
+import Typography from '@material-ui/core/Typography';
 
+import AlertBox from "../../../../../../../../common/AlertBox";
+import StyledButton from "../../../../../../../../common/StyledButton";
 import { useStyles } from "./styles";
 
 const PrivateKey = ({ classes }) => {
-  return <span>PrivateKey</span>
+  return (
+    <div className={classes.privateKeyContainer}>
+      <Typography variant="body2" className={classes.successMsg}>Successfully Created Wallet for : Service Provider 1</Typography>
+      <p className={classes.description}><span>Information about the wallet and key. </span>Lorem ipsum dolor sit amet, vim natum dolore cu. It will take some time for the tokens to reflect on your channel. Please go ahead.</p>
+      <div className={classes.downloadKeyBtn}>
+        <StyledButton type="transparent" btnText="download private key"/>
+      </div>
+      <AlertBox type="warning" message="Please keep the private key secured. Once the private key is lost, it can not be recovered." />
+      <div className={classes.btnContainer}>
+        <StyledButton type="transparent" btnText="cancel"/>
+        <StyledButton type="blue" disabled btnText="continue"/>
+      </div>
+    </div>
+  )  
 };
 
 export default withStyles(useStyles)(PrivateKey);
--- src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/CreateWalletPopup/index.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/CreateWalletPopup/index.js
@@ -22,7 +22,7 @@ import { useStyles } from "./styles";
 class CreateWalletPopup extends Component {
 	state = {
     progressText: ["Details", "Purchase", "Private Key", "Summary"],
-    activeSection: 2
+    activeSection: 3
   }
 
 	handleCancel = () => {
