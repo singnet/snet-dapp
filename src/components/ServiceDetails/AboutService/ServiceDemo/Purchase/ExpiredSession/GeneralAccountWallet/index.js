a3e01ee45f4aa88b392aa1775198b7382112c11e
Vivek <n.vin95@gmail.com>
2019-09-26 14:27:38 +0530

    onClick of create Wallet

--- src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/index.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/index.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { withStyles } from "@material-ui/styles";
 
 import StyledButton from "../../../../../../common/StyledButton";
 import { useStyles } from "./styles";
+import CreateWalletPopup from "./CreateWalletPopup";
 
-const GeneralAccountWallet = ({ classes }) => (
-  <div className={classes.btnsContainer}>
-    <StyledButton type="transparentBlueBorderDisable" btnText="transaction history" />
-    <StyledButton type="transparentBlueBorderDisable" btnText="top up wallet" />
-    <StyledButton
-      type="blue"
-      btnText="create wallet"
-      //    onClick={this.handleCreateWallet}
-    />
-  </div>
-);
+const GeneralAccountWallet = ({ classes }) => {
+  const [showCreateWalletPopup, setShowCreateWalletPopup] = useState(false);
+  return (
+    <Fragment>
+      <div className={classes.btnsContainer}>
+        <StyledButton type="transparentBlueBorderDisable" btnText="transaction history" />
+        <StyledButton type="transparentBlueBorderDisable" btnText="top up wallet" />
+        <StyledButton type="blue" btnText="create wallet" onClick={() => setShowCreateWalletPopup(true)} />
+      </div>
+      <CreateWalletPopup open={showCreateWalletPopup} handleClose={() => setShowCreateWalletPopup(false)} />
+    </Fragment>
+  );
+};
 
 export default withStyles(useStyles)(GeneralAccountWallet);
