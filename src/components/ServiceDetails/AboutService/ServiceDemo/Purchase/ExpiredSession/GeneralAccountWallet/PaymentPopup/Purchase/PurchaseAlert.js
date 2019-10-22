0f2400c94110bd2fb018766de4db69376332624a
Vivek205 <n.vin95@gmail.com>
2019-10-22 17:25:56 +0530

    added orderId and error.message to the support mail

--- src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/PaymentPopup/Purchase/PurchaseAlert.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/PaymentPopup/Purchase/PurchaseAlert.js
@@ -5,13 +5,24 @@ import AlertBox from "../../../../../../../../common/AlertBox";
 import StyledButton from "../../../../../../../../common/StyledButton";
 import { useStyles } from "./styles";
 
-const PurchaseAlert = ({ classes, alert, handleCancel }) => {
+const generateSupportMail = (orderId, errorMsg) => {
+  const subject = `Order id: ${orderId}`;
+  const body = `Error: ${errorMsg}%0d%0aPlease brief your issue below:%0d%0a...`;
+  return `mailto:${process.env.REACT_APP_SNET_SUPPORT_MAIL}?subject=${subject}&body=${body}`;
+};
+
+const PurchaseAlert = ({ classes, alert, handleCancel, orderId }) => {
   return (
     <div className={classes.purchaseErrorContainer}>
       <AlertBox type={alert.type} message={alert.message} />
       <div className={classes.btnContainer}>
         <StyledButton type="transparent" btnText="cancel" onClick={handleCancel} />
-        <StyledButton type="transparent" btnText="support" href={`mailto:${process.env.REACT_APP_SNET_SUPPORT_MAIL}`} />
+        <StyledButton
+          type="transparent"
+          btnText="support"
+          href={generateSupportMail(orderId, alert.mailerErrContent)}
+          newTab
+        />
       </div>
     </div>
   );
