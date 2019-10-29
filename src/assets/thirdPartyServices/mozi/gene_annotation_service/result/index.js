e7f38eafee281e367e9d32cba2db13e8a891bc4d
Yisehak <yisehak.aw@gmail.com>
2019-10-29 22:57:32 +0300

    implement summary table

--- src/assets/thirdPartyServices/mozi/gene_annotation_service/result/index.js
+++ src/assets/thirdPartyServices/mozi/gene_annotation_service/result/index.js
@@ -9,7 +9,18 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import CloudDownloadIcon from "@material-ui/icons/CloudDownloadOutlined";
 import TableChartOutlinedIcon from "@material-ui/icons/TableChartOutlined";
 import VisibilityIcon from "@material-ui/icons/VisibilityOutlined";
+import AssessmentIcon from "@material-ui/icons/AssessmentOutlined";
 import "./style.css";
+import Modal from "@material-ui/core/Modal";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+const width = document.body.clientWidth || window.screen.width;
 
 export const AnnotationStatus = {
   ACTIVE: 1,
@@ -22,6 +33,9 @@ const AnnotationResult = props => {
   const [isTableShown, setTableShown] = useState(false);
   const [isVisualizerShown, setVisualizerShown] = useState(false);
   const [isFetchingResult, setFetchingResult] = useState(false);
+  const [isSummaryShown, setSummaryShown] = useState(false);
+  const [summaryTab, setSummaryTab] = useState(0);
+  const [summary, setSummary] = useState(undefined);
   const { ACTIVE, COMPLETED, ERROR } = AnnotationStatus;
   const id = props.id;
 
@@ -80,10 +94,26 @@ const AnnotationResult = props => {
         <Typography variant="body2">
           The result contains {nodes.length} entities and {edges.length} connections between them.
         </Typography>
-        {/* <Typography variant="body2">
-          This page will expire in {formatDistanceToNow(toDate(response.expire_time * 1000))}.
-        </Typography> */}
         <div className="inline-buttons">
+          <Button
+            variant="contained"
+            onClick={e => {
+              if (!summary) {
+                fetch(`${RESULT_ADDR}/summary/${id}`).then(data => {
+                  data
+                    .clone()
+                    .text()
+                    .then(t => {
+                      console.log(t);
+                      setSummary(JSON.parse(t));
+                    });
+                });
+              }
+              setSummaryShown(true);
+            }}
+          >
+            <AssessmentIcon style={{ marginRight: 15 }} /> View summary
+          </Button>
           <Button variant="contained" onClick={e => setTableShown(true)}>
             <TableChartOutlinedIcon style={{ marginRight: 15 }} /> View results table
           </Button>
@@ -94,16 +124,91 @@ const AnnotationResult = props => {
             <VisibilityIcon style={{ marginRight: 15 }} /> Visualize the result
           </Button>
         </div>
-        {/* <Typography variant="body2" className="call-to-action">
-          <Button color="primary">
-            <ChevronLeft />
-            Run another annotation
-          </Button>
-        </Typography> */}
-        {/* Show the visualizer */}
       </Fragment>
     );
   };
+
+  const renderSummaryTable = tableData => {
+    const rows = Object.values(tableData).reduce((acc, v) => ({ ...acc, ...v[0] }), {});
+    return (
+      <Table className="mozi modal-table" size="small" style={{ minWidth: width - 300 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            {Object.keys(rows).map(r => (
+              <TableCell>{r.split("_").join(" ")}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.keys(tableData).map((k, i) => (
+            <TableRow>
+              <TableCell>{k}</TableCell>
+              {Object.keys(rows).map(r => (
+                <TableCell>{tableData[k][0][r] || "-"}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    );
+  };
+
+  const renderSummary = data => (
+    <Modal className="mozi modal" onClose={() => setSummaryShown(false)} open={true}>
+      {data ? (
+        <div className="content">
+          <Typography variant="body2" style={{ padding: "20px 15px" }}>
+            A Reference Databases: <a href={data["A Reference Databases"]}>{data["A Reference Databases"]}</a>
+          </Typography>
+          <AppBar position="static" color="default">
+            <Tabs indicatorColor="primary" value={summaryTab} onChange={(e, value) => setSummaryTab(value)}>
+              {Object.keys(data)
+                .filter((d, i) => i)
+                .map((d, i) => (
+                  <Tab key={`k${i}`} label={d} id={`tab${i}`} />
+                ))}
+            </Tabs>
+            <div
+              style={{
+                position: "absolute",
+                textAlign: "right",
+                right: 15,
+                top: 15,
+              }}
+            >
+              {summary && (
+                <Button
+                  size="small"
+                  variant="outlined"
+                  color="primary"
+                  onClick={() => {
+                    let json = JSON.stringify(summary);
+                    const link = document.createElement("a");
+                    let file = new Blob([json], { type: "text/json" });
+                    link.href = URL.createObjectURL(file);
+                    link.download = `summary.json`;
+                    document.body.appendChild(link);
+                    link.click();
+                    link.remove();
+                  }}
+                >
+                  <CloudDownloadIcon style={{ marginRight: 15 }} /> Download summary JSON
+                </Button>
+              )}
+            </div>
+          </AppBar>
+          {renderSummaryTable(Object.values(data)[summaryTab + 1])}
+        </div>
+      ) : (
+        <div style={{ padding: 30 }}>
+          <Typography variant="body1">
+            <CircularProgress /> Fetching summary ...
+          </Typography>
+        </div>
+      )}
+    </Modal>
+  );
 
   return (
     <div className="content-wrapper">
@@ -137,6 +242,8 @@ const AnnotationResult = props => {
           handleClose={() => setTableShown(false)}
         />
       )}
+      {response && console.log("response.csv_files", response.csv_files)}
+      {isSummaryShown && renderSummary(summary)}
     </div>
   );
 };
