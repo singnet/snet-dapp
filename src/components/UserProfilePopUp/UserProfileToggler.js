ffa8ea41a614fcbe5545ad76d23ded76973772cd
Vivek205 <n.vin95@gmail.com>
2019-11-19 16:19:53 +0530

    removed Blockies

--- src/components/UserProfilePopUp/UserProfileToggler.js
+++ src/components/UserProfilePopUp/UserProfileToggler.js
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import Icon from "@material-ui/core/Icon";
 import { withStyles } from "@material-ui/styles";
 import clsx from "clsx";
-import { connect } from "react-redux";
 
 import { useStyles } from "./styles";
 import UserProfilePopUp from "./";
-import UserBlockies from "../common/UserBlockies";
 
-const UserProfileToggler = ({ classes, email }) => {
+const UserProfileToggler = ({ classes }) => {
   const [showProfilePopup, setShowProfilePopup] = useState(false);
 
   const handleClick = () => {
@@ -19,15 +17,11 @@ const UserProfileToggler = ({ classes, email }) => {
     return (
       <div className={classes.loggedInActions} onClick={handleClick}>
         <Icon className={clsx(classes.icon, "fas fa-bell")} />
-        <UserBlockies seed={email} className={classes.reactBlockies} size={8} />
+        <Icon className={clsx(classes.icon, "fas fa-user-circle")} />
       </div>
     );
   }
   return <UserProfilePopUp handleClick={handleClick} />;
 };
 
-const mapStateToProps = state => ({
-  email: state.userReducer.email,
-});
-
-export default connect(mapStateToProps)(withStyles(useStyles)(UserProfileToggler));
+export default withStyles(useStyles)(UserProfileToggler);
