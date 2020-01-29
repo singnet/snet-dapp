525b2042697beb6e8e6cdb1c3db1106414ad6398
arturgontijo <arturgontijo@gmail.com>
2020-01-29 14:26:51 -0300

    [s2vt-video-captioning] Adding back links.

--- src/assets/thirdPartyServices/snet/s2vt_video_captioning/index.js
+++ src/assets/thirdPartyServices/snet/s2vt_video_captioning/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import InfoIcon from "@material-ui/icons/Info";
 
+import HoverIcon from "../../standardComponents/HoverIcon";
 import OutlinedTextArea from "../../common/OutlinedTextArea";
 
 import { VideoCaptioning } from "./video_cap_pb_service";
@@ -17,7 +20,7 @@ export default class S2VTVideoCaptioning extends React.Component {
     this.state = {
       users_guide:
         "https://github.com/singnet/dnn-model-services/blob/master/docs/users_guide/s2vt-video-captioning.md",
-      code_repo: "https://github.com/singnet/dnn-model-services/blob/master/Services/gRPC/s2vt-video-captioning",
+      code_repo: "https://github.com/singnet/dnn-model-services/tree/master/services/s2vt-video-captioning",
       reference: "https://vsubhashini.github.io/s2vt.html",
 
       serviceName: "VideoCaptioning",
@@ -127,6 +130,30 @@ export default class S2VTVideoCaptioning extends React.Component {
               onChange={this.handleFormUpdate}
               onFocus={this.handleFocus}
             />
+          </Grid>
+
+          <Grid item xs container justify="flex-end">
+            <Grid item>
+              <HoverIcon text="View code on Github" href={this.state.code_repo}>
+                <SvgIcon>
+                  <path // Github Icon
+                    d="M12.007 0C6.12 0 1.1 4.27.157 10.08c-.944 5.813 2.468 11.45 8.054 13.312.19.064.397.033.555-.084.16-.117.25-.304.244-.5v-2.042c-3.33.735-4.037-1.56-4.037-1.56-.22-.726-.694-1.35-1.334-1.756-1.096-.75.074-.735.074-.735.773.103 1.454.557 1.846 1.23.694 1.21 2.23 1.638 3.45.96.056-.61.327-1.178.766-1.605-2.67-.3-5.462-1.335-5.462-6.002-.02-1.193.42-2.35 1.23-3.226-.327-1.015-.27-2.116.166-3.09 0 0 1.006-.33 3.3 1.23 1.966-.538 4.04-.538 6.003 0 2.295-1.5 3.3-1.23 3.3-1.23.445 1.006.49 2.144.12 3.18.81.877 1.25 2.033 1.23 3.226 0 4.607-2.805 5.627-5.476 5.927.578.583.88 1.386.825 2.206v3.29c-.005.2.092.393.26.507.164.115.377.14.565.063 5.568-1.88 8.956-7.514 8.007-13.313C22.892 4.267 17.884.007 12.008 0z"
+                  />
+                </SvgIcon>
+              </HoverIcon>
+            </Grid>
+            <Grid item>
+              <HoverIcon text="User's guide" href={this.state.users_guide}>
+                <InfoIcon />
+              </HoverIcon>
+            </Grid>
+            <Grid item>
+              <HoverIcon text="View original project" href={this.state.reference}>
+                <SvgIcon>
+                  <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 11.701c0 2.857-1.869 4.779-4.5 5.299l-.498-1.063c1.219-.459 2.001-1.822 2.001-2.929h-2.003v-5.008h5v3.701zm6 0c0 2.857-1.869 4.779-4.5 5.299l-.498-1.063c1.219-.459 2.001-1.822 2.001-2.929h-2.003v-5.008h5v3.701z" />
+                </SvgIcon>
+              </HoverIcon>
+            </Grid>
           </Grid>
 
           <Grid item xs={12} style={{ textAlign: "center" }}>
