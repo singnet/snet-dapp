7e4c0796cc6ba7a23f2651e5b129b599a1a8806c
Artur Gontijo <arturgontijo@gmail.com>
2020-04-15 16:02:40 +0000

    Fixing typo.

--- src/Redux/actionCreators/ServiceActions.js
+++ src/Redux/actionCreators/ServiceActions.js
@@ -118,7 +118,7 @@ export const downloadAuthToken = (serviceId, groupId, publicKey, orgId) => async
 
     const jsonToDownload = {
       email: userId,
-      tokeToMakeFreeCall: data.token_to_make_free_call,
+      tokenToMakeFreeCall: data.token_to_make_free_call,
       tokenExpirationBlock: data.token_expiration_block,
     };
     const downloadBlob = new Blob([JSON.stringify(jsonToDownload)], { type: "text/json;charset=utf-8" });
