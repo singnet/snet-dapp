84b951b44d3aa6215f77f42923f07fc51b8dd207
dasari-ananya <ananya.dasari@nirman.io>
2021-04-20 15:20:07 +0530

    added download files button, desription

--- src/components/ServiceDetails/InstallAndRunService/Python/index.js
+++ src/components/ServiceDetails/InstallAndRunService/Python/index.js
@@ -3,38 +3,47 @@ import { withStyles } from "@material-ui/styles";
 
 import { useStyles } from "../styles";
 import CodeSnippet from "../../../common/CodeSnippet";
+import StyledButton from "../../../common/StyledButton";
 
-const Python = ({ classes }) => {
+const Python = ({ classes, description }) => {
   return (
     <section className={classes.languageTabSection}>
-      <h2>Install</h2>
-      <div>
-        <span>Install the SDK using PIP</span>
-        <CodeSnippet>pip install snet.sdk</CodeSnippet>
+      <div className={classes.descriptionBtnsContainer}>
+        <p>{description}</p>
+        <div className={classes.btnContainer}>
+          <StyledButton type="blue" btnText="Download Integration files" />
+        </div>
       </div>
-      <div>
-        <span>
-          To generate the gRPC client libraries, you need the SingularityNET Command Line Interface, or CLI, which you
-          can install using pip
-        </span>
-        <CodeSnippet>pip install snet-cli</CodeSnippet>
-      </div>
-      <div>
-        <span>Once you have the CLI installed, run the following command to generate gRPC stubs for service </span>
-        <CodeSnippet>snet sdk generate-client-library python &lt;org_id&gt; &lt;service_id&gt;</CodeSnippet>
-      </div>
-      <div>
-        <span>Run the code</span>
-        <CodeSnippet>
-          from snet.sdk import SnetSDK <br />
-          import &lt;stub&gt;_pb2 <br />
-          import &lt;stub&gt;_pb2_grpc <br />
-          from config import config <br /> <br />
-          sdk = SnetSDK(config) <br />
-          service_client = sdk.create_service_client( <br />
-          &lt;org_id&gt;, &lt;service_id&gt;, example_service_pb2_grpc.CalculatorStub) <br />
-          request = example_service_pb2.Numbers(a=20, b=3) <br />
-        </CodeSnippet>
+      <div className={classes.setingUpFilesContainer}>
+        <h3>Setting Up Files</h3>
+        <div>
+          <span>Install the SDK using PIP</span>
+          <CodeSnippet>pip install snet.sdk</CodeSnippet>
+        </div>
+        <div>
+          <span>
+            To generate the gRPC client libraries, you need the SingularityNET Command Line Interface, or CLI, which you
+            can install using pip
+          </span>
+          <CodeSnippet>pip install snet-cli</CodeSnippet>
+        </div>
+        <div>
+          <span>Once you have the CLI installed, run the following command to generate gRPC stubs for service </span>
+          <CodeSnippet>snet sdk generate-client-library python &lt;org_id&gt; &lt;service_id&gt;</CodeSnippet>
+        </div>
+        <div>
+          <span>Run the code</span>
+          <CodeSnippet>
+            from snet.sdk import SnetSDK <br />
+            import &lt;stub&gt;_pb2 <br />
+            import &lt;stub&gt;_pb2_grpc <br />
+            from config import config <br /> <br />
+            sdk = SnetSDK(config) <br />
+            service_client = sdk.create_service_client( <br />
+            &lt;org_id&gt;, &lt;service_id&gt;, example_service_pb2_grpc.CalculatorStub) <br />
+            request = example_service_pb2.Numbers(a=20, b=3) <br />
+          </CodeSnippet>
+        </div>
       </div>
     </section>
   );
