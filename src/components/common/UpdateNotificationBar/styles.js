4b4a34e38b350635ad32b2aee8c4e5b2dea10c58
dasari-ananya <ananya.dasari@nirman.io>
2021-05-05 19:48:44 +0530

    replaced announcment icon from img tag

--- src/components/common/UpdateNotificationBar/styles.js
+++ src/components/common/UpdateNotificationBar/styles.js
@@ -1,12 +1,13 @@
 export const useStyles = theme => ({
   updateNotificationBar: {
-    padding: "8px 0",
+    padding: "2px 0",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     position: "relative",
     backgroundColor: "rgba(64,134,255,0.5)",
     color: theme.palette.text.white,
+    "& img": { marginRight: 12 },
     "& p": {
       margin: 0,
       fontSize: 14,
@@ -20,9 +21,6 @@ export const useStyles = theme => ({
         textDecoration: "none",
       },
       "& svg": { fontSize: 12 },
-    },
-    "& svg": {
-      "&:first-of-type": { marginRight: 12 },
     },
   },
   closeIcon: {
