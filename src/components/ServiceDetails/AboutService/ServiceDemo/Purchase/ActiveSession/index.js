44db6434b10d14c230312565f9a827434cf2b46c
Vivek205 <ichbinvivek@gmail.com>
2021-05-26 17:56:03 +0530

    disabled free call

--- src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ActiveSession/index.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ActiveSession/index.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import { withStyles } from "@material-ui/styles";
 import Tooltip from "@material-ui/core/Tooltip";
 
-import AlertBox from "../../../../../common/AlertBox";
+import AlertBox, { alertTypes } from "../../../../../common/AlertBox";
 import StyledButton from "../../../../../common/StyledButton";
 import StyledLinearProgress from "../../../../../common/StyledLinearProgress";
 import { useStyles } from "./styles";
+import { Fragment } from "react";
 
 const ActiveSession = ({ classes, freeCallsRemaining, handleComplete, freeCallsAllowed, isServiceAvailable }) => {
   const [showTooltip, setShowTooltip] = useState(false);
@@ -33,6 +34,24 @@ const ActiveSession = ({ classes, freeCallsRemaining, handleComplete, freeCallsA
         <span className={classes.FreeApiCallsText}>Free API Calls</span>
         <span className={classes.ReaminaingCallsNo}>{freeCallsRemaining}</span>
         <StyledLinearProgress value={progressValue()} />
+        <AlertBox
+          type={alertTypes.WARNING}
+          message={
+            <Fragment>
+              <span>
+                We have temporarily disabled this action as we are hard forking the AGI token to make it cross
+                compatible. We will enable it as soon as the hard fork is completed. Read more{" "}
+              </span>
+              <a
+                href="https://blog.singularitynet.io/singularitynet-phase-ii-launch-sequence-activated-agi-token-to-be-hard-forked-to-10ede4b6c89"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                here
+              </a>
+            </Fragment>
+          }
+        />
         <Tooltip
           title="Service is currently offline. Please try after sometime"
           aria-label="add-payment"
@@ -42,7 +61,13 @@ const ActiveSession = ({ classes, freeCallsRemaining, handleComplete, freeCallsA
           classes={{ tooltip: classes.tooltip }}
         >
           <div>
-            <StyledButton type="blue" btnText="run for free" onClick={handleComplete} disabled={!isServiceAvailable} />
+            <StyledButton
+              type="blue"
+              btnText="run for free"
+              onClick={handleComplete}
+              // disabled={!isServiceAvailable}
+              disabled
+            />
           </div>
         </Tooltip>
       </div>
