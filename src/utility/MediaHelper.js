519b5a123c19bef27b24582132ba81c051174646
Dhivakhar V <dhivakharjv@gmail.com>
2021-07-06 17:03:17 +0530

    clean up

--- src/utility/MediaHelper.js
+++ src/utility/MediaHelper.js
@@ -1,7 +1,7 @@
 const DownloadMedia = (media, asset_type, filename) => {
-    const stubDetails = media.filter(media_item => media_item.asset_type == asset_type)[0]
-    if (typeof (stubDetails) !== "undefined") {
-        const url = stubDetails['url']
+    const mediaDetails = media.filter(media_item => media_item.asset_type == asset_type)[0]
+    if (typeof (mediaDetails) !== "undefined") {
+        const url = mediaDetails['url']
         const link = document.createElement('a')
         link.href = url
         link.setAttribute('download', filename)
