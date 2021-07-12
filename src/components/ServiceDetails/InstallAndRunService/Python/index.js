c48d9ba529fe8cf2f8ae5a3a8e8a6107d8247136
Dhivakhar V <dhivakharjv@gmail.com>
2021-07-12 16:18:34 +0530

    Install and run tab || documentation clean up

--- src/components/ServiceDetails/InstallAndRunService/Python/index.js
+++ src/components/ServiceDetails/InstallAndRunService/Python/index.js
@@ -35,23 +35,6 @@ const downloadIntegrationFiles = () => {
           <CodeSnippet>pip install snet-cli</CodeSnippet>
         </div>
         <div>
-          <span>Once you have the CLI installed, run the following command to generate gRPC stubs for service </span>
-          <CodeSnippet>snet sdk generate-client-library python &lt;org_id&gt; &lt;service_id&gt;</CodeSnippet>
-        </div>
-        <div>
-          <span>Run the code</span>
-          <CodeSnippet>
-            from snet.sdk import SnetSDK <br />
-            import &lt;stub&gt;_pb2 <br />
-            import &lt;stub&gt;_pb2_grpc <br />
-            from config import config <br /> <br />
-            sdk = SnetSDK(config) <br />
-            service_client = sdk.create_service_client( <br />
-            &lt;org_id&gt;, &lt;service_id&gt;, example_service_pb2_grpc.CalculatorStub) <br />
-            request = example_service_pb2.Numbers(a=20, b=3) <br />
-          </CodeSnippet>
-        </div>
-        <div>
           <span>Run the code</span>
           <CodeSnippet>
             from snet.sdk import SnetSDK <br />
@@ -64,8 +47,8 @@ const downloadIntegrationFiles = () => {
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"private_key": "&#60;your wallet's private key&#62;", <br />
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"eth_rpc_endpoint": "https://ropsten.infura.io/v3/&#60;your infura
             key&#62;", <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"org_id": {orgId}, <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"service_id": {serviceId}, <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"org_id": "{orgId}", <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"service_id": "{serviceId}", <br />
             &nbsp;&nbsp;&nbsp;&nbsp;&#125;
             <br />
             &nbsp;&nbsp;&nbsp;&nbsp;sdk = SnetSDK(config=config) <br />
