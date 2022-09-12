60d7822c3731e1ad491d39c0cc77f36e366bb0dd
dasari-ananya <ananya.dasari@nirman.io>
2022-09-12 13:22:54 +0530

    Added the payment mode UI

--- src/components/ServiceDetails/TrainingModels/CreateModel/Payment/index.js
+++ src/components/ServiceDetails/TrainingModels/CreateModel/Payment/index.js
@@ -1,29 +1,33 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
 import Grid from "@material-ui/core/Grid";
-import StyledButton from "../../../../common/StyledButton";
 import Typography from "@material-ui/core/Typography";
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+
 import { useStyles } from "./styles";
+import PaymentMode from "./PaymentMode";
 
 const Payment = ({ classes }) => {
+
+  const addEllipsisAtEndOfString = str => `${str.substr(0, 40)}...`;
+
   return (
     <div className={classes.paymentContaienr}>
       <div className={classes.reviewReqContainer}>
         <span>Review request</span>
         <Grid container>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
             <span>Default Model:</span>
           </Grid>
-          <Grid item xs={10}>
+          <Grid item xs={9}>
             <Typography>Yes</Typography>
           </Grid>
         </Grid>
         <Grid container>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
             <span>Model name:</span>
           </Grid>
-          <Grid item xs={10}>
+          <Grid item xs={9}>
             <Typography>
               Animal detection
               <EditOutlinedIcon />
@@ -31,10 +35,10 @@ const Payment = ({ classes }) => {
           </Grid>
         </Grid>
         <Grid container>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
             <span>Model description:</span>
           </Grid>
-          <Grid item xs={10}>
+          <Grid item xs={9}>
             <Typography>
               Helping wildlife researchers in studying the wild animal species collectively and making strategies to
               protect them. Artificial intelligence tracks wildlife patterns and predicts the extinction of endangered
@@ -44,10 +48,10 @@ const Payment = ({ classes }) => {
           </Grid>
         </Grid>
         <Grid container>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
             <span>Data set name:</span>
           </Grid>
-          <Grid item xs={10}>
+          <Grid item xs={9}>
             <Typography>
               Butterflies & Caterpillers
               <EditOutlinedIcon />
@@ -55,10 +59,10 @@ const Payment = ({ classes }) => {
           </Grid>
         </Grid>
         <Grid container>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
             <span>Data files:</span>
           </Grid>
-          <Grid item xs={10}>
+          <Grid item xs={9}>
             <Typography>
               <a href="#" title="Zipped File Name">
                 https://www.littlebigfiles.com/file234565432123454321.zip
@@ -68,16 +72,27 @@ const Payment = ({ classes }) => {
           </Grid>
         </Grid>
         <Grid container>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
+            <span>Access for this model:</span>
+          </Grid>
+          <Grid item xs={9}>
+            <Typography>
+              Public(5)
+              <EditOutlinedIcon />
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={3}>
             <span>Cardano ID:</span>
           </Grid>
-          <Grid item xs={10}>
+          <Grid item xs={9}>
             <ul>
-              <li>456iorkjahfjouo23eyu3o2u01982409un9u092....</li>
-              <li>456iorkjahfjouo23eyu3o2u01982409un9u092....</li>
-              <li>456iorkjahfjouo23eyu3o2u01982409un9u092....</li>
-              <li>456iorkjahfjouo23eyu3o2u01982409un9u092....</li>
-              <li>456iorkjahfjouo23eyu3o2u01982409un9u092....</li>
+              <li>{addEllipsisAtEndOfString("456iorkjahfjouo23eyu3o2u01982409un9u092")}</li>
+              <li>{addEllipsisAtEndOfString("456iorkjahfjouo23eyu3o2u01982409un9u092")}</li>
+              <li>{addEllipsisAtEndOfString("456iorkjahfjouo23eyu3o2u01982409un9u092")}</li>
+              <li>{addEllipsisAtEndOfString("456iorkjahfjouo23eyu3o2u01982409un9u092")}</li>
+              <li>{addEllipsisAtEndOfString("456iorkjahfjouo23eyu3o2u01982409un9u092")}</li>
             </ul>
           </Grid>
         </Grid>
@@ -89,10 +104,8 @@ const Payment = ({ classes }) => {
           <span>agix tokens</span>
           <span>0.002</span>
         </div>
-        <div className={classes.paymentMode}>
-          <p>Please select a payment method to continue</p>
-        </div>
       </div>
+      <PaymentMode />
     </div>
   );
 };
