5dc0b7a330aee5a3bc524245c4985b5b08fb3302
dasari-ananya <ananya.dasari@nirman.io>
2022-09-19 13:23:13 +0530

    removed commented code

--- src/components/ServiceDetails/TrainingModels/CreateModel/Data/Upload/index.js
+++ src/components/ServiceDetails/TrainingModels/CreateModel/Data/Upload/index.js
@@ -1,37 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { withStyles } from "@material-ui/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import AppBar from "@material-ui/core/AppBar";
 import UploadFromLink from "./UploadFromLink";
 import { useStyles } from "./styles";
 
-const Data = ({ classes }) => {
-  const [value, setValue] = React.useState(0);
-  const [selectedTab, setSelectedTab] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-		setSelectedTab(newValue)
-  };
-
-  return (
-    <UploadFromLink />
-    // <AppBar position="static" className={classes.appBar}>
-    //   <Tabs
-    //     className={classes.tabsContainer}
-    //     value={value}
-    //     onChange={handleChange}
-    //     variant="scrollable"
-    //     indicatorColor="primary"
-    //     textColor="primary"
-    //     aria-label="basic tabs example"
-    //   >
-    //     <Tab className={classes.tab} label="Upload from link" value={0} />
-    //   </Tabs>
-    //   {selectedTab === 0 && ( <UploadFromLink /> )}
-    // </AppBar>
-  );
+const Data = () => {
+  return <UploadFromLink />;
 };
 
 export default withStyles(useStyles)(Data);
