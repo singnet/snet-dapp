ed4bc67d0229d68e1f7005031504a95928e2c844
sreeshnap <sreeshna.p.vijayan@gmail.com>
2022-09-27 09:19:19 +0530

    UI data for create model training

--- src/components/ServiceDetails/TrainingModels/CreateModel/index.js
+++ src/components/ServiceDetails/TrainingModels/CreateModel/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { withStyles } from "@material-ui/styles";
 
 import { useStyles } from "./styles";
@@ -8,13 +8,111 @@ import Data from "./Data";
 import Payment from "./Payment";
 import Finish from "./Finish";
 
-const CreateModel = ({ classes, training }) => {
+import { connect, useDispatch } from "react-redux";
+import { initSdk } from "../../../../utility/sdk";
+import { loaderActions, userActions } from "../../../../Redux/actionCreators";
+import { LoaderContent } from "../../../../utility/constants/LoaderContent";
+import { walletTypes } from "../../../../Redux/actionCreators/UserActions";
+import ConnectMetamask from "../../ConnectMetamask";
+import { currentServiceDetails, groupInfo } from "../../../../Redux/reducers/ServiceDetailsReducer";
+import { WebServiceClient as ServiceClient } from "snet-sdk-web";
+
+const CreateModel = ({ classes, training ,
+ 
+  startMMconnectLoader,
+  fetchAvailableUserWallets,
+  stopLoader,
+  registerWallet,
+  updateWallet,
+
+  wallet,
+  serviceDetails,
+  groupInfo,
+  haveANewModel,
+
+}) => {
+
+  const [metamaskConnected, setMetamaskConnected] = useState(false);
   const [activeSection, setActiveSection] = React.useState(1);
 
   const handleNextClick = () => {
-    setActiveSection(activeSection + 1);
+    handleConnectMM();
+    setActiveSection(activeSection + 1);   
   };
 
+  const handleNextClickCreateModel =  ()=>{
+    create_model();
+    setActiveSection(activeSection + 1);
+  }
+
+  const handleConnectMM = async () => {
+    try {
+      startMMconnectLoader();
+      const sdk = await initSdk();
+      const address = await sdk.account.getAddress();
+      const availableUserWallets = await fetchAvailableUserWallets();
+      const addressAlreadyRegistered = availableUserWallets.some(wallet => wallet.address.toLowerCase() === address);
+
+      if (!addressAlreadyRegistered) {
+        await registerWallet(address, walletTypes.METAMASK);
+      }
+      updateWallet({ type: walletTypes.METAMASK, address });
+    } catch (error) {
+      console.log("===error==", error);
+    }
+  };
+
+  useEffect(() => {
+    if (wallet.address) {
+      // dispatch(loaderActions.startAppLoader(LoaderContent.FETCH_TRAINING_EXISTING_MODEL));
+      // dispatch(loaderActions.startAppLoader(LoaderContent.CREATE_TRAINING_MODEL));
+      createANewOne();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [wallet]);
+
+  const dispatch = useDispatch();
+  const createANewOne = async () => {
+    const sdk = await initSdk();
+    const { org_id, service_id } = serviceDetails;
+    const serviceClient = new ServiceClient(sdk, org_id, service_id, sdk._mpeContract, {}, groupInfo);
+    setServiceClientState(serviceClient);
+    setMetamaskConnected(true);
+    stopLoader();
+  };
+  console.log(ethAddress,'ethaddress');
+  //create_model for Nexttt
+  const create_model = async () => {
+    // const sdk = await initSdk();
+    // const { org_id, service_id } = serviceDetails;
+    // const serviceClient = new ServiceClient(sdk, org_id, service_id, sdk._mpeContract, {}, groupInfo);
+    
+    console.log(trainingModelDescription,'description');
+    console.log(trainingDataLink,"trainingDataLink");
+    // console.log(ethAddress,'ethaddress');
+    const createModel = await createModelForTraining(serviceClientState);
+    console.log(createModel,"----createModel---");
+
+    // setMetamaskConnected(true);
+    // stopLoader();
+  };
+  const createModelForTraining = async (serviceClientState) =>{
+    const create_model = await serviceClientState.create_model(trainingMethod,wallet.address,trainingModelServiceName,trainingModelDescription,enableAccessModel,ethAddress,trainingDataLink);
+    return create_model;
+    
+  }
+
+    
+  //copy
+  const [enableAccessModel, setEnableAccessModel] = useState(false);
+  const [ethAddress, setEthAddress] = useState([]);
+  const [trainingMethod, setTrainingMethod] = useState(undefined);
+  const [trainingModelServiceName, setTrainingModelServiceName] = useState("");
+  const [trainingModelDescription, setTrainingModelDescription] = useState("");
+  const [trainingDataLink,setTrainingDataLink] = useState("");
+  const [serviceClientState,setServiceClientState] = useState({});
+  //
+  
   const onBackClick = () => {
     setActiveSection(activeSection - 1);
   };
@@ -22,11 +120,22 @@ const CreateModel = ({ classes, training }) => {
   const createModelTabs = [
     {
       key: "modelInfo",
-      component: <ModelInfo handleNextClick={handleNextClick} training={training} />,
+      component: <ModelInfo handleNextClick={handleNextClick} training={training}
+      trainingMethod={trainingMethod} setTrainingMethod={setTrainingMethod}
+      trainingModelServiceName={trainingModelServiceName} setTrainingModelServiceName={setTrainingModelServiceName}
+      trainingModelDescription={trainingModelDescription} setTrainingModelDescription={setTrainingModelDescription}
+      enableAccessModel={enableAccessModel} setEnableAccessModel={setEnableAccessModel}
+      ethAddress={ethAddress} setEthAddress={setEthAddress}
+      metamaskConnected={metamaskConnected}  setMetamaskConnected={setMetamaskConnected}
+      handleConnectMM={handleConnectMM}
+      />,
     },
     {
       key: "data",
-      component: <Data handleNextClick={handleNextClick} onBackClick={onBackClick} />,
+      component: <Data handleNextClick={handleNextClick} onBackClick={onBackClick}
+      trainingDataLink={trainingDataLink} setTrainingDataLink={setTrainingDataLink}
+      handleNextClickCreateModel={handleNextClickCreateModel}
+      />,
     },
     {
       key: "payment",
@@ -57,4 +166,25 @@ const CreateModel = ({ classes, training }) => {
   );
 };
 
-export default withStyles(useStyles)(CreateModel);
+//create model
+
+const mapStateToProps = state => ({
+  wallet: state.userReducer.wallet,
+  serviceDetails: currentServiceDetails(state),
+  groupInfo: groupInfo(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  startMMconnectLoader: () => dispatch(loaderActions.startAppLoader(LoaderContent.CONNECT_METAMASK)),
+  fetchAvailableUserWallets: () => dispatch(userActions.fetchAvailableUserWallets()),
+  registerWallet: (address, type) => dispatch(userActions.registerWallet(address, type)),
+  updateWallet: ({ type, address }) => dispatch(userActions.updateWallet({ type, address })),
+  stopLoader: () => dispatch(loaderActions.stopAppLoader),
+});
+
+
+// export default withStyles(useStyles)(CreateModel);
+export default connect(mapStateToProps, mapDispatchToProps) (withStyles(useStyles)(CreateModel));
+
+
+
