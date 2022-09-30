2813351bceae8a81e6ec40435d964a1c9ac41d72
sreeshnap <sreeshna.p.vijayan@gmail.com>
2022-09-30 13:15:20 +0530

    delete model update

--- src/components/ServiceDetails/ExistingModel/index.js
+++ src/components/ServiceDetails/ExistingModel/index.js
@@ -82,7 +82,6 @@ const ExistingModel = ({
             <ModelDetails
               title="Region Recognition"
               id={model.modelId}
-              idd={model}
               description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley."
               status="Inprogress"
               accessTo="Public"
