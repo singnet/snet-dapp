294919ebb1999381f02cd2b72b2ea5154273ce22
sreeshnap <sreeshna.p.vijayan@gmail.com>
2022-10-13 11:24:11 +0530

    Upgrade marketplace dapp in to Goerli network

--- src/config/Networks.js
+++ src/config/Networks.js
@@ -1,9 +1,11 @@
 export const Networks = {
   1: "Mainnet",
   3: "Ropsten Test",
+  5: "Goerli Test",
 };
 
 export const networkName = {
   1: "mainnet",
-  3: "ropsten"
+  3: "ropsten",
+  5: "goerli",
 }
