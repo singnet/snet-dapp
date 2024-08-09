d579edaf06469253e3590b40b77927097d9c6fc2
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-08-09 15:09:56 +0300

    [SPT-518] unnecessary import has been deleted

--- src/components/AiMarketplace/index.js
+++ src/components/AiMarketplace/index.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import { connect } from "react-redux";
 import MainSection from "./MainSection";
 import SeoMetadata from "../common/SeoMetadata";
 import ServiceListingHeader from "./ServiceListingHeader";
