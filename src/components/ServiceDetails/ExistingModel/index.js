fe58725bc52ffdd7149cad298ff0a11a6196a543
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-11-20 14:18:30 +0300

    [SPT-738] updated get models button view

--- src/components/ServiceDetails/ExistingModel/index.js
+++ src/components/ServiceDetails/ExistingModel/index.js
@@ -1,17 +1,16 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { withStyles } from "@mui/styles";
-import ModelDetails from "./ModelDetails";
 import { useStyles } from "./styles";
-import ConnectMetamask from "../ConnectMetamask";
+import ModelDetails from "./ModelDetails";
 import { loaderActions, userActions } from "../../../Redux/actionCreators";
 import { LoaderContent } from "../../../utility/constants/LoaderContent";
 import { currentServiceDetails } from "../../../Redux/reducers/ServiceDetailsReducer";
-import Typography from "@mui/material/Typography";
 import AlertBox, { alertTypes } from "../../common/AlertBox";
 import Card from "../../common/Card";
 import { getTrainingModels } from "../../../Redux/actionCreators/ServiceTrainingActions";
-import { isEmpty } from "lodash";
+import { isUndefined } from "lodash";
+import StyledButton from "../../common/StyledButton";
 
 const ExistingModel = ({ classes, openEditModel }) => {
   const { org_id, service_id } = useSelector((state) => currentServiceDetails(state));
@@ -19,7 +18,6 @@ const ExistingModel = ({ classes, openEditModel }) => {
   const { address } = useSelector((state) => state.userReducer.wallet);
 
   const [existingModels, setExistingModels] = useState(modelsList);
-  const [metamaskConnected, setMetamaskConnected] = useState(!isEmpty(address));
   const [alert, setAlert] = useState({});
   const dispatch = useDispatch();
 
@@ -33,7 +31,6 @@ const ExistingModel = ({ classes, openEditModel }) => {
       dispatch(loaderActions.startAppLoader(LoaderContent.CONNECT_METAMASK));
       const address = await dispatch(userActions.updateMetamaskWallet());
       await dispatch(getTrainingModels(org_id, service_id, address));
-      setMetamaskConnected(true);
     } catch (error) {
       setAlert({ type: alertTypes.ERROR, message: "Unable to fetch existing models. Please try again" });
       dispatch(loaderActions.stopAppLoader());
@@ -41,10 +38,10 @@ const ExistingModel = ({ classes, openEditModel }) => {
   };
 
   const ModelList = () => {
-    if (!existingModels.length) {
+    if (!existingModels?.length) {
       return (
         <div className={classes.noDataFoundTxt}>
-          <Typography>No data found</Typography>
+          <h2>No data found</h2>
         </div>
       );
     }
@@ -62,11 +59,11 @@ const ExistingModel = ({ classes, openEditModel }) => {
     <Card
       header="Existing Model"
       children={
-        metamaskConnected ? (
+        !isUndefined(modelsList) ? (
           <ModelList />
         ) : (
           <Fragment>
-            <ConnectMetamask handleConnectMM={handleConnectMM} />
+            <StyledButton btnText="Get models" onClick={handleConnectMM} />
             <AlertBox type={alert.type} message={alert.message} />
           </Fragment>
         )
