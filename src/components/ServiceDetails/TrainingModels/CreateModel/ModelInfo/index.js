af402db10ef0e0bf23dbca0472cb68b0f3f3c95d
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-11-27 17:00:36 +0300

    [SPT-737] Removed selecting of training method

--- src/components/ServiceDetails/TrainingModels/CreateModel/ModelInfo/index.js
+++ src/components/ServiceDetails/TrainingModels/CreateModel/ModelInfo/index.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 
 // import FormControlLabel from "@mui/material/FormControlLabel";
 // import Switch from "@mui/material/Switch";
-import StyledDropdown from "../../../../common/StyledDropdown";
 import StyledTextField from "../../../../common/StyledTextField";
 import StyledButton from "../../../../common/StyledButton";
 
@@ -24,20 +23,20 @@ const ModelInfo = ({ classes, cancelEditModel }) => {
   const { currentModel } = useSelector((state) => state.serviceTrainingReducer);
   const { org_id, service_id } = useSelector((state) => currentServiceDetails(state));
 
+  // const [trainingMethod, setTrainingMethod] = useState(currentModel ? currentModel.methodName : undefined);
   //eslint-disable-next-line
   const [isRestrictAccessModel, setIsRestrictAccessModel] = useState(
     true
     // currentModel && currentModel.publicAccess ? true : false
   );
   const [accessAddresses, setAccessAddresses] = useState(currentModel ? currentModel.addressList : []);
-  const [trainingMethod, setTrainingMethod] = useState(currentModel ? currentModel.methodName : undefined);
   const [trainingModelName, setTrainingServiceName] = useState(currentModel ? currentModel.modelName : "");
   const [trainingModelDescription, setTrainingModelDescription] = useState(
     currentModel ? currentModel.description : ""
   );
   const [trainingDataLink, setTrainingDataLink] = useState(currentModel ? currentModel.dataLink : "");
   const [alert, setAlert] = useState({});
-
+  const trainingMethod = detailsTraining?.trainingMethods[0];
   // const onUpdate = async () => {
   //   const updateModelParams = {
   //     trainingModelName,
@@ -91,19 +90,19 @@ const ModelInfo = ({ classes, cancelEditModel }) => {
   //   setIsRestrictAccessModel(!isRestrictAccessModel);
   // };
 
-  const trainingModelAccess = detailsTraining?.trainingMethods || [];
+  // const trainingModelAccess = detailsTraining?.trainingMethods || [];
 
-  const trainingDropDownObject = trainingModelAccess.map((e) => ({
-    value: e,
-    label: e,
-  }));
+  // const trainingDropDownObject = trainingModelAccess.map((e) => ({
+  //   value: e,
+  //   label: e,d
+  // }));
 
-  const trainingMethodDropDownBox = (event) => {
-    const { value } = event.target;
-    if (value !== "default") {
-      setTrainingMethod(value);
-    }
-  };
+  // const trainingMethodDropDownBox = (event) => {
+  //   const { value } = event.target;
+  //   if (value !== "default") {
+  //     setTrainingMethod(value);
+  //   }
+  // };
 
   const handleModelServiceName = (event) => {
     setTrainingServiceName(event.target.value);
@@ -131,13 +130,13 @@ const ModelInfo = ({ classes, cancelEditModel }) => {
     <div className={classes.modelInfoContaienr}>
       <div className={classes.trainingBasicDetails}>
         <div className={classes.methodDropBox}>
-          <StyledDropdown
+          {/*<StyledDropdown
             inputLabel="Training Method"
             list={trainingDropDownObject}
             value={trainingMethod}
             onChange={trainingMethodDropDownBox}
           />
-          <span>Please select a method to train as a first step.</span>
+          {/* <span>Please select a method to train as a first step.</span> */}
         </div>
         <div className={classes.modelNameContainer}>
           <StyledTextField label="Model name" value={trainingModelName} onChange={handleModelServiceName} />
