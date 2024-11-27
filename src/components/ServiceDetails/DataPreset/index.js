57a260e5d32fd14b08509e849099f3995f0907f1
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-11-27 18:04:16 +0300

    [SPT-767] added navigate to training model from dataset editor

--- src/components/ServiceDetails/DataPreset/index.js
+++ src/components/ServiceDetails/DataPreset/index.js
@@ -9,14 +9,26 @@ import AddIcon from "@mui/icons-material/Add";
 import StyledButton from "../../common/StyledButton";
 import clsx from "clsx";
 import { isEmpty } from "lodash";
+import { useLocation, useNavigate } from "react-router-dom";
+import { setCurrentModelDetails } from "../../../Redux/actionCreators/ServiceTrainingActions";
+import { useDispatch } from "react-redux";
 
 const DataPreset = ({ classes }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+
   const [datasetInfo, setDatasetInfo] = useState();
   const [datasetForMergeInfo, setDatasetForMergeInfo] = useState();
 
   const cleanMainDataset = () => {
     setDatasetInfo(datasetForMergeInfo);
     setDatasetForMergeInfo();
+  };
+
+  const goToCreateModel = () => {
+    dispatch(setCurrentModelDetails({ dataset: datasetInfo }));
+    navigate(location.pathname.split("tab/")[0] + "tab/" + 3, { state: { isOpenCreatingModel: true } });
   };
 
   const DataPresetContainer = () => {
@@ -59,7 +71,7 @@ const DataPreset = ({ classes }) => {
           </div>
         </div>
         <div className={classes.fineTuneBatton}>
-          <StyledButton disabled={isEmpty(datasetInfo)} btnText="Fine-Tune" />
+          <StyledButton disabled={isEmpty(datasetInfo)} btnText="Fine-Tune" onClick={goToCreateModel} />
         </div>
       </div>
     );
