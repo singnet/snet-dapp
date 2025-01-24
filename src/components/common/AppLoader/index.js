22214c6f91289592898c3f237c262b64777eb042
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2025-01-24 17:48:10 +0300

    [SPT-446] fixed app modal loader

--- src/components/common/AppLoader/index.js
+++ src/components/common/AppLoader/index.js
@@ -2,33 +2,23 @@ import React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import CardHeader from "@mui/material/CardHeader";
-import Card from "@mui/material/Card";
-import Modal from "@mui/material/Modal";
-import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
 
 import { useStyles } from "./styles";
+import SNETDialog from "../SNETDialog";
 
 export const AppLoader = ({ loading, loaderHeader, loaderText }) => {
   const classes = useStyles();
 
   return (
-    <Modal open={loading}>
-      <Card className={classes.card}>
-        <CardHeader title={<h2>{loaderHeader}</h2>} />
-        <Divider />
-        <div className={classes.circularProgressContainer}>
-          <CircularProgress className={classes.circularProgress} />
-        </div>
-        <CardContent>
-          <Typography variant="body2" component="p">
-            {loaderText}
-          </Typography>
-        </CardContent>
-      </Card>
-    </Modal>
+    <SNETDialog isDialogOpen={loading} showCloseButton={false} title={loaderHeader}>
+      <div className={classes.circularProgressContainer}>
+        <CircularProgress className={classes.circularProgress} />
+      </div>
+      <Typography variant="body2" component="p">
+        {loaderText}
+      </Typography>
+    </SNETDialog>
   );
 };
 
