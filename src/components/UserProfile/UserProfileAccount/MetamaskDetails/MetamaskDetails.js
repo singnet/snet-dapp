50077c85c78303fd4843b1b789c7d257287d4144
Marina Fedyantseva <marina.fedyantzeva@singularitynet.io>
2025-02-18 14:25:56 +0300

    [SPT-446] removed info icons withot tooltips

--- src/components/UserProfile/UserProfileAccount/MetamaskDetails/MetamaskDetails.js
+++ src/components/UserProfile/UserProfileAccount/MetamaskDetails/MetamaskDetails.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { withStyles } from "@mui/styles";
 import { useStyles } from "./styles";
 
-import InfoIcon from "@mui/icons-material/Info";
 import { cogsToAgi } from "../../../../utility/PricingStrategy";
 import { loaderActions, sdkActions } from "../../../../Redux/actionCreators";
 import { LoaderContent } from "../../../../utility/constants/LoaderContent";
@@ -48,28 +47,24 @@ const MetamaskDetails = ({ classes }) => {
     <div className={classes.accountDetails}>
       <div>
         <div className={classes.label}>
-          <InfoIcon />
           <span>Current Network</span>
         </div>
         <span>{currentNetwork} Network</span>
       </div>
       <div>
         <div className={classes.label}>
-          <InfoIcon />
           <span>Wallet ID</span>
         </div>
         <span className={classes.walletId}>{wallet.address}</span>
       </div>
       <div className={classes.bgBox}>
         <div className={classes.label}>
-          <InfoIcon />
           <span>Total Tokens</span>
         </div>
         <span>{tokenBalance} AGIX</span>
       </div>
       <div className={classes.bgBox}>
         <div className={classes.label}>
-          <InfoIcon />
           <span>Escrow Balance</span>
         </div>
         <span>{escrowBalance} AGIX</span>
