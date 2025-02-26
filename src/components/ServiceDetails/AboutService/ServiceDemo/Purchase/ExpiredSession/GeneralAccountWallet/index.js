076ecc244137c77d8975d206182e283608ee46e0
Marina Fedyantseva <marina.fedyantzeva@singularitynet.io>
2025-02-26 17:43:43 +0300

    [SM-6] added loader and reworked action buttons for general wallet

--- src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/index.js
+++ src/components/ServiceDetails/AboutService/ServiceDemo/Purchase/ExpiredSession/GeneralAccountWallet/index.js
@@ -1,52 +1,111 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { withStyles } from "@mui/styles";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
 
 import StyledButton from "../../../../../../common/StyledButton";
 import { useStyles } from "./styles";
 import NextAction from "./NextAction";
 import { userProfileRoutes } from "../../../../../../UserProfile";
-import { anyPendingTxn as getAnyPendingTxn } from "../../../../../../../Redux/reducers/PaymentReducer";
-import {
-  channelInfo as getChannelInfo,
-  anyGeneralWallet as getAnyGeneralWallet,
-} from "../../../../../../../Redux/reducers/UserReducer";
+import { channelInfo as getChannelInfo } from "../../../../../../../Redux/reducers/UserReducer";
 import PaymentPopup from "./PaymentPopup";
-import { orderTypes } from "../../../../../../../utility/constants/PaymentConstants";
+import { orderPayloadTypes, orderTypes } from "../../../../../../../utility/constants/PaymentConstants";
 import { isEmpty } from "lodash";
+import PaymentInfoCard from "../../PaymentInfoCard";
+import AlertBox, { alertTypes } from "../../../../../../common/AlertBox";
+import {
+  anyPendingTxn as getAnyPendingTxn,
+  anyFailedTxn as getAnyFailedTxn,
+} from "../../../../../../../Redux/reducers/PaymentReducer";
+import { userActions } from "../../../../../../../Redux/actionCreators";
+import { groupInfo } from "../../../../../../../Redux/reducers/ServiceDetailsReducer";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { initPaypalSdk } from "../../../../../../../utility/sdk";
+
+const TransactionAlert = {
+  PENDING: { type: alertTypes.WARNING, message: "Transaction Confirmed. Pending token allocation" },
+  FAILED: { type: alertTypes.ERROR, message: "Transaction Failed. See history for more details" },
+};
 
 const GeneralAccountWallet = ({ classes, handleContinue }) => {
-  const anyGeneralWallet = useSelector((state) => getAnyGeneralWallet(state));
-  const anyPendingTxn = useSelector((state) => getAnyPendingTxn(state));
-  const channelInfo = getChannelInfo();
+  const dispatch = useDispatch();
+  const { orgId } = useParams();
 
-  const [paymentPopupVisibile, setPaymentPopupVisibile] = useState();
+  const channelInfo = useSelector((state) => getChannelInfo(state));
+  const anyPendingTxn = useSelector((state) => getAnyPendingTxn(state));
+  const anyFailedTxn = useSelector((state) => getAnyFailedTxn(state));
+  const groupId = useSelector((state) => groupInfo(state).group_id);
+  const inProgressOrderType = useSelector((state) => state.paymentReducer.paypalInProgress.orderType);
+
+  const progressTransaction = Object.keys(orderPayloadTypes).find(
+    (key) => orderPayloadTypes[key] === inProgressOrderType
+  );
+  const [paymentPopupVisibile, setPaymentPopupVisibile] = useState(progressTransaction);
+  const [alert, setAlert] = useState({});
+  const [isLoadingChannelInfo, setLoadingChannelInfo] = useState(false);
+
+  useEffect(() => {
+    if (process.env.REACT_APP_SANDBOX) {
+      return;
+    }
+    initPaypalSdk(channelInfo.address, channelInfo.id); // TODO get address
+  }, [channelInfo]);
+
+  useEffect(() => {
+    if (anyPendingTxn) {
+      setAlert(TransactionAlert.PENDING);
+    }
+    if (anyFailedTxn) {
+      setAlert(TransactionAlert.FAILED);
+    }
+    setAlert({});
+  }, [anyPendingTxn, anyFailedTxn]);
+
+  useEffect(() => {
+    const getWalletInfo = async () => {
+      try {
+        setLoadingChannelInfo(true);
+        await dispatch(userActions.fetchWallet(orgId, groupId));
+      } catch (error) {
+        console.error("error: ", error);
+      } finally {
+        setLoadingChannelInfo(false);
+      }
+    };
+    getWalletInfo();
+  }, [dispatch, orgId, groupId]);
 
   return (
     <Fragment>
+      <div className={classes.channelBalance}>
+        <PaymentInfoCard
+          show={!isEmpty(channelInfo)}
+          title="Channel Balance"
+          value={!isEmpty(channelInfo) && channelInfo.balanceInAgi}
+          unit="AGIX"
+        />
+      </div>
       <div className={classes.btnsContainer}>
         <Link to={userProfileRoutes.TRANSACTIONS} target="_blank" className={classes.routerLink}>
           <StyledButton type="transparentBlueBorder" btnText="transaction history" />
         </Link>
-        <StyledButton
-          type="transparentBlueBorder"
-          btnText="top up wallet"
-          onClick={() => setPaymentPopupVisibile(orderTypes.TOPUP_WALLET)}
-          disabled={isEmpty(channelInfo)}
-        />
-        <NextAction
-          channel={channelInfo}
-          setShowCreateWalletPopup={() => setPaymentPopupVisibile(orderTypes.CREATE_WALLET)}
-          setShowLinkProvider={() => setPaymentPopupVisibile(orderTypes.CREATE_CHANNEL)}
-          handleContinue={handleContinue}
-          anyPendingTxn={anyPendingTxn}
-          anyGeneralWallet={anyGeneralWallet}
-        />
+        {isLoadingChannelInfo ? (
+          <CircularProgress size="40px" />
+        ) : (
+          <NextAction
+            channel={channelInfo}
+            setShowCreateWalletPopup={() => setPaymentPopupVisibile(orderTypes.CREATE_WALLET)}
+            setShowLinkProvider={() => setPaymentPopupVisibile(orderTypes.CREATE_CHANNEL)}
+            setShowTopUpWallet={() => setPaymentPopupVisibile(orderTypes.TOPUP_WALLET)}
+            handleContinue={handleContinue}
+          />
+        )}
+        <AlertBox {...alert} />
       </div>
       <PaymentPopup
         paymentModalType={paymentPopupVisibile}
         isVisible={Boolean(paymentPopupVisibile)}
+        isPaypalInProgress={Boolean(progressTransaction)}
         handleClose={() => setPaymentPopupVisibile(false)}
       />
     </Fragment>
