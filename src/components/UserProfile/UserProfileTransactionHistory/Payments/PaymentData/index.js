55e12789a24060e21d4a1793465b3a3767ecd3be
Marina Fedyantseva <marina.fedyantzeva@singularitynet.io>
2025-02-26 16:25:05 +0300

    [SPT-446] fixed transaction history table

--- src/components/UserProfile/UserProfileTransactionHistory/Payments/PaymentData/index.js
+++ src/components/UserProfile/UserProfileTransactionHistory/Payments/PaymentData/index.js
@@ -5,6 +5,7 @@ import Typography from "@mui/material/Typography";
 
 import AlertText from "../../../../common/AlertText";
 import { useStyles } from "./styles";
+import { alertTypes } from "../../../../common/AlertBox";
 
 const PaymentData = ({
   classes,
@@ -19,28 +20,33 @@ const PaymentData = ({
   itemQuantity,
   itemUnit,
 }) => {
+  const paymentStatusType = {
+    PENDING: alertTypes.WARNING,
+    NOT_SUBMITTED: alertTypes.INFO,
+  };
+
   return (
     <Grid item xs={12} sm={12} md={12} lg={12} className={classes.paymentsDataRow}>
-      <Grid item xs={2} sm={2} md={2} lg={2}>
+      <Grid item xs={1} sm={1} md={1} lg={1}>
         <div className={classes.dateTime}>
           <Typography>{date}</Typography>
           <Typography>{time}</Typography>
         </div>
       </Grid>
-      <Grid item xs={2} sm={2} md={2} lg={2}>
+      <Grid item xs={4} sm={4} md={4} lg={4}>
         <div className={classes.providerNameID}>
           <Typography>{organizationName}</Typography>
           <Typography className={classes.providersId}>ID-{orderId}</Typography>
         </div>
       </Grid>
-      <Grid item xs={2} sm={2} md={2} lg={2}>
+      {/* <Grid item xs={2} sm={2} md={2} lg={2}>
         <Typography>{paymentChannel}</Typography>
-      </Grid>
+      </Grid> */}
       <Grid item xs={3} sm={3} md={3} lg={3}>
         <Typography>{orderType}</Typography>
       </Grid>
-      <Grid item xs={1} sm={1} md={1} lg={1} className={classes.alertBox}>
-        <AlertText type={statusType} message={statusType} />
+      <Grid item xs={2} sm={2} md={2} lg={2} className={classes.alertBox}>
+        <AlertText type={paymentStatusType[statusType]} message={statusType} />
       </Grid>
       <Grid item xs={1} sm={1} md={1} lg={1}>
         <Typography>${cost}</Typography>
