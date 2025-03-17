fd692345b1a4712f0df1e3b2e9d7513be6e23fcd
Marina Fedyantseva <marina.fedyantzeva@singularitynet.io>
2025-03-17 18:21:44 +0300

    [SM-37] fixed install and run tab

---
+++ src/components/ServiceDetails/InstallAndRunService/IntegrationFilesActions/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { withStyles } from "@mui/styles";
+import { useSelector } from "react-redux";
+import { useStyles } from "../styles";
+import StyledButton from "../../../common/StyledButton";
+import DownloadMedia, { isMediaAvailableForDownloading } from "../../../../utility/MediaHelper";
+import { tabsContent } from "../TabsMeta";
+
+const IntegrationFilesActions = ({ classes, tabName }) => {
+  const { media } = useSelector((state) => state.serviceDetailsReducer.details);
+  const content = tabsContent[tabName];
+
+  const downloadIntegrationFiles = () => {
+    DownloadMedia(media, content.assetType, content.fileName);
+  };
+
+  const description = `Download the ${content.projectName} to help you integrate this AI service with your application. Once you setup your configuration, use the token generator below to test the servcie with a number of free calls.`;
+  const isDownloadAvailable = isMediaAvailableForDownloading(media, content.assetType);
+
+  return (
+    <section className={classes.languageTabSection}>
+      <div className={classes.descriptionBtnsContainer}>
+        <p>{description}</p>
+        <div className={classes.btnContainer}>
+          <StyledButton
+            type="blue"
+            btnText="Download Integration files"
+            onClick={downloadIntegrationFiles}
+            disabled={!isDownloadAvailable}
+          />
+          <StyledButton type="transparent" btnText="view tutorial" href={content.docsLink} target="_blank" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default withStyles(useStyles)(IntegrationFilesActions);
---
+++ src/components/ServiceDetails/InstallAndRunService/TabsMeta.js
@@ -0,0 +1,21 @@
+export const tabNames = {
+  PYTHON: "python",
+  NODEJS: "nodejs",
+};
+
+export const tabsContent = {
+  python: {
+    projectName: "Python SDK",
+    docsLink:
+      "https://dev.singularitynet.io/docs/products/DecentralizedAIPlatform/SDK/PythonSDK/getting-started-guide/",
+    assetType: "grpc-stub/python",
+    fileName: "python.zip",
+  },
+  nodejs: {
+    projectName: "NodeJS SDK",
+    docsLink:
+      "https://dev.singularitynet.io/docs/products/DecentralizedAIPlatform/SDK/JavascriptSDKs/NodeJsSDK/getting-started-guide/",
+    assetType: "grpc-stub/nodejs",
+    fileName: "nodejs.zip",
+  },
+};
