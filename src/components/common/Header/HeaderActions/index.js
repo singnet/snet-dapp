f3de693978d27bc5b3c6319da141d996a15845a9
Marina Fedyantseva <marie.saez@mail.ru>
2025-05-06 17:40:52 +0300

    [SM-68] add buy fet button

--- src/components/common/Header/HeaderActions/index.js
+++ src/components/common/Header/HeaderActions/index.js
@@ -4,16 +4,18 @@ import { useStyles } from "./styles";
 import UserProfileToggler from "../../../UserProfilePopUp/UserProfileToggler";
 import LoginActionsBtns from "./LoginActionsBtns";
 import TokenPurchase from "../../../TokenPurchase/TokenPurchase";
+import { TOKEN_NAMES } from "../../../../utility/constants/EthereumUtils";
 
 const HeaderActions = () => {
   const classes = useStyles();
   const isLoggedIn = useSelector((state) => state.userReducer.login.isLoggedIn);
+  const isFetToken = process.env.REACT_APP_TOKEN_NAME === TOKEN_NAMES.FET;
 
   return (
     <div className={classes.loginBtnsContainer}>
       {isLoggedIn ? (
         <Fragment>
-          <TokenPurchase />
+          {isFetToken && <TokenPurchase />}
           <UserProfileToggler />
         </Fragment>
       ) : (
