7d91d71fb66942bedef0e5bf4b3efaed4b16329e
Marina Fedyantseva <marie.saez@mail.ru>
2025-05-15 13:23:59 +0300

    [SM-85] move app loader to a separate component

---
+++ src/components/common/AppCircularLoader/index.js
@@ -0,0 +1,12 @@
+import CircularProgress from "@mui/material/CircularProgress";
+import "./style.css";
+
+const AppCircularLoader = () => {
+  return (
+    <div className="loader-container">
+      <CircularProgress />
+    </div>
+  );
+};
+
+export default AppCircularLoader;
