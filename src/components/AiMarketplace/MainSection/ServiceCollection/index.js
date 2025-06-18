e4f4d57af9008ad0550399b358a7fca8ee64f3c6
Marina Fedyantseva <marie.saez@mail.ru>
2025-06-18 15:33:10 +0300

    [SM-43] refactor list of service component

--- src/components/AiMarketplace/MainSection/ServiceCollection/index.js
+++ src/components/AiMarketplace/MainSection/ServiceCollection/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { connect } from "react-redux";
 import CircularProgress from "@mui/material/CircularProgress";
 import truncate from "lodash/truncate";
 
@@ -8,11 +7,17 @@ import ServiceListItem from "./ServiceListItem";
 import { useStyles } from "./styles";
 import Routes from "../../../../utility/constants/Routes";
 import GridViewItem from "./GridViewItem";
+import { useSelector } from "react-redux";
 
 const maxDescriptionChars = 180;
 
-const CardGroup = ({ data: cards = [], listView, loading }) => {
+const CardGroup = ({ listView }) => {
   const classes = useStyles();
+
+  const services = useSelector((state) => state.serviceReducer.services);
+  const loading = useSelector((state) => state.loaderReducer.aiServieList);
+
+  const cards = services ? services : [];
 
   if (loading) {
     return (
@@ -24,6 +29,7 @@ const CardGroup = ({ data: cards = [], listView, loading }) => {
       </div>
     );
   }
+
   if (cards.length === 0) {
     return (
       <div className={classes.NoResultContainer}>
@@ -33,38 +39,30 @@ const CardGroup = ({ data: cards = [], listView, loading }) => {
     );
   }
 
+  const cardPropsGenerate = (card) => {
+    return {
+      cardMedia: card.media.url,
+      orgImg: card.org_assets_url.hero_image,
+      cardTitle: card.display_name,
+      cardSubheader: card.organization_name,
+      ratingGiven: card.service_rating,
+      cardDescription: truncate(card.short_description, { length: maxDescriptionChars }),
+      isAvailable: Boolean(card.is_available),
+    };
+  };
+
   return (
     <div className={listView ? classes.listViewCardCollection : classes.gridViewCardCollection}>
       {cards.map((card) => {
+        const cardProps = cardPropsGenerate(card);
+
         return (
           <Link
             key={card.org_id + card.service_id}
             to={`/${Routes.SERVICE_DETAILS}/org/${card.org_id}/service/${card.service_id}/tab/0`} //TODO
             className={classes.routerLink}
           >
-            {listView ? (
-              <ServiceListItem
-                cardMedia={card.media.url}
-                orgImg={card.org_assets_url.hero_image}
-                cardTitle={card.display_name}
-                cardSubheader={card.organization_name}
-                ratingGiven={card.service_rating}
-                totalRating={card.total_users_rated}
-                cardDescription={truncate(card.short_description, { length: maxDescriptionChars })}
-                isAvailable={Boolean(card.is_available)}
-              />
-            ) : (
-              <GridViewItem
-                cardMedia={card.media.url}
-                orgImg={card.org_assets_url.hero_image}
-                cardTitle={card.display_name}
-                cardSubheader={card.organization_name}
-                ratingGiven={card.service_rating}
-                totalRating={card.total_users_rated}
-                cardDescription={truncate(card.short_description, { length: maxDescriptionChars })}
-                isAvailable={Boolean(card.is_available)}
-              />
-            )}
+            {listView ? <ServiceListItem {...cardProps} /> : <GridViewItem {...cardProps} />}
           </Link>
         );
       })}
@@ -72,8 +70,4 @@ const CardGroup = ({ data: cards = [], listView, loading }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  loading: state.loaderReducer.aiServieList,
-});
-
-export default connect(mapStateToProps)(CardGroup);
+export default CardGroup;
