07e2e8a54542688ba68f66553b8d6331825af543
Marina Fedyantseva <marie.saez@mail.ru>
2025-06-30 16:25:30 +0300

    [SM-139] update user delete form style

--- src/components/UserProfile/UserProfileSettings/ConfirmDelete/index.js
+++ src/components/UserProfile/UserProfileSettings/ConfirmDelete/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "@mui/material/Modal";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
@@ -6,17 +6,13 @@ import CloseIcon from "@mui/icons-material/Close";
 import IconButton from "@mui/material/IconButton";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
 
 import { useStyles } from "./styles";
 import StyledButton from "../../../common/StyledButton";
 import BulletPoint from "../../../common/BulletPoint";
 import AlertBox, { alertTypes } from "../../../common/AlertBox";
+import StyledDropdown from "@common/StyledDropdown";
 
 const warningMessage = [
   "Your wallet or any wallets you have used will remain in your possession. We do not have any access to your wallet and cannot help you recover wallet keys.",
@@ -24,18 +20,18 @@ const warningMessage = [
   "All personal data associated with your account will be deleted from our records.",
 ];
 
-const resonForLeaving = [
-  { label: "I want to create another account", value: " " },
-  { label: "I am dissatisfied with the platform", value: " " },
-  { label: "There were not enough services", value: " " },
-  { label: "It is too difficult to use", value: " " },
-  { label: "It is too slow to use", value: " " },
-  { label: "Other (Please describe below)", value: " " },
+const reasonsForLeaving = [
+  { label: "I want to create another account", value: "0" },
+  { label: "I am dissatisfied with the platform", value: "1" },
+  { label: "There were not enough services", value: "2" },
+  { label: "It is too difficult to use", value: "3" },
+  { label: "It is too slow to use", value: "4" },
+  { label: "Other (Please describe below)", value: "5" },
 ];
 
 const ConfirmDelete = ({ open, handleClose, handleSubmit, error }) => {
   const classes = useStyles();
-  const [reasonForLeaving, setReasonForLeaving] = React.useState("");
+  const [reasonForLeaving, setReasonForLeaving] = useState();
 
   const handleCancel = () => {
     setReasonForLeaving("");
@@ -74,24 +70,19 @@ const ConfirmDelete = ({ open, handleClose, handleSubmit, error }) => {
               </div>
               <AlertBox type={alertTypes.ERROR} message={error} />
             </div>
-            <div className={classes.inputFieldContainer}>
-              <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel htmlFor="outlined-age-simple">Help us improve, tell us why you are leaving</InputLabel>
-                <Select value={reasonForLeaving} onChange={handleChange} input={<OutlinedInput />}>
-                  {resonForLeaving.map((item) => (
-                    <MenuItem className={classes.menuItem} key={item.label} value={item.label}>
-                      {item.label}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <TextField
-                label="Your feedback matters. Anything else you’d like share "
-                multiline
-                rows="7"
-                variant="outlined"
-              />
-            </div>
+            <StyledDropdown
+              list={reasonsForLeaving}
+              value={reasonForLeaving}
+              inputLabel="Help us improve, tell us why you are leaving"
+              name="resonForLeaving"
+              onChange={handleChange}
+            />
+            <TextField
+              label="Your feedback matters. Anything else you’d like share"
+              multiline
+              rows="7"
+              variant="outlined"
+            />
           </CardContent>
           <CardActions className={classes.CardActions}>
             <StyledButton btnText="cancel" type="transparent" onClick={handleCancel} />
