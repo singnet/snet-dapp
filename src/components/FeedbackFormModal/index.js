f3410c07d47aecfa99aabb5ccee47f7267392f42
Marina Fedyantseva <marie.saez@mail.ru>
2026-01-20 13:45:48 +0300

    [SM-254] remove old captcha implementation

--- src/components/FeedbackFormModal/index.js
+++ src/components/FeedbackFormModal/index.js
@@ -1,25 +1,15 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useState } from "react";
 import OpenFormButton from "./OpenFormButton";
 import "./styles.css";
 import FeedbackFormModal from "./FeedbackFormModal";
 import { sendFeedbackSnetAPI } from "../../config/SupportAPI";
-import CaptchaModal from "../Captcha/modal/CaptchaModal";
-import { loadCaptchaScript } from "../Captcha/WAFScriptLoad";
 
 const FeedbackForm = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  useEffect(() => {
-    try {
-      loadCaptchaScript();
-    } catch (err) {
-      console.error("CAPTCHA script error: ", err);
-    }
-  }, []);
-
   return (
     <Fragment>
-      <CaptchaModal />
+      <div id="captchaContainer" />
       <FeedbackFormModal
         sendFeedbackAPI={sendFeedbackSnetAPI}
         isModalVisible={isModalVisible}
